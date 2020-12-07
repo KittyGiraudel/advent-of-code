580de5753b61cd001bebc89c78e511fd5b6740b2
Kitty Giraudel <kitty.giraudel@gmail.com>
2020-12-07 12:06:36 +0100

    Update day 2

--- src/day-2/index.js
+++ src/day-2/index.js
@@ -1,13 +1,16 @@
-const validateLoose = ([, i, j, letter, password]) => {
-  const occurrences = (password.match(new RegExp(letter, 'g')) || []).length
-  return occurrences >= Number(i) && occurrences <= Number(j)
+const validateLoose = ([i, j, letter, password]) => {
+  const occurrences = password.match(new RegExp(letter, 'g'))?.length ?? 0
+  return occurrences >= i && occurrences <= j
 }
-const validateStrict = ([, i, j, letter, password]) =>
-  [i, j]
-    .map(index => password[Number(index) - 1])
-    .filter(char => char === letter).length === 1
-const parsePolicy = policy => policy.match(/(\d+)-(\d+) (\w): ([^$]+)/)
+
+const validateStrict = ([i, j, letter, password]) =>
+  [i, j].map(index => password[index - 1]).filter(char => char === letter)
+    .length === 1
+
+const parsePolicy = policy => policy.split(/\W+/)
 const isValidLoose = policy => validateLoose(parsePolicy(policy))
 const isValidStrict = policy => validateStrict(parsePolicy(policy))
+
+// (s,[n,x,l,p]=s.split(/\W+/))=>!!((p[n-1]==l)^(p[x-1]==l))
 
 module.exports = { isValidLoose, isValidStrict }
