a586771806f7f3f62669972e19259c20392ef85c
Kitty Giraudel <kitty.giraudel@gmail.com>
2020-12-07 16:03:21 +0100

    Update day 1

--- src/day-1/index.js
+++ src/day-1/index.js
@@ -1,6 +1,24 @@
-const getCombinations = require('../helpers/getCombinations')
 const sum = require('../helpers/sum')
-const product = require('../helpers/product')
+
+// Return all possible unique combinations of size `n` from given `array`.
+// @param {Array} array - Array to compute combinations for
+// @param {Number} n - Combination size
+// @param {Array} start - Internal
+// @param {Array} tmp - Internal
+// @return {Array}
+const getCombinations = (array, n, start = [], tmp = []) =>
+  array.reduce((acc, item, index, array) => {
+    if (n > 1) {
+      tmp.push(item)
+      getCombinations(array.slice(index + 1), n - 1, acc, tmp)
+    } else {
+      acc.push((tmp.push(item), tmp).slice(0))
+    }
+
+    tmp.pop()
+
+    return acc
+  }, start)
 
 // Find set of `size` items from `input` array summing to 2020.
 // @param {Number[]} input - Array of numbers
