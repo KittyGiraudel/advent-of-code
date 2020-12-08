bc3dccd641271572a7761e5932350e39c44bd027
Kitty Giraudel <hugo.giraudel@gmail.com>
2020-12-08 10:30:01 +0100

    Add day 8

---
+++ src/day-8/index.js
@@ -0,0 +1,68 @@
+const updateAtIndex = require('../helpers/updateAtIndex')
+
+// Execute the given array of instructions.
+// @param {String[]} instructions - List of instructions making the program
+// @return {Object} `accumulator` value and 0 or 1 `exit` code
+const runProgram = instructions => {
+  const history = []
+  let accumulator = 0
+  let pointer = 0
+
+  // As long as we have an instruction to execute and it has not been executed
+  // yet, keep running.
+  while (instructions[pointer] && !history.includes(pointer)) {
+    history.push(pointer)
+
+    const [operation, argument] = instructions[pointer].split(' ')
+
+    switch (operation) {
+      case 'acc':
+        accumulator += Number(argument)
+        pointer++
+        break
+      case 'jmp':
+        pointer += Number(argument)
+        break
+      case 'nop':
+        pointer++
+        break
+      default:
+    }
+  }
+
+  return { accumulator, exit: Number(pointer < instructions.length) }
+}
+
+// Patch given instruction by making it `jmp` instruction if it’s a `nop` one or
+// `nop` instruction if it’s a `jmp` one.
+// @param {String} line - Line to patch
+// @return {String} Patched line
+const patchInstruction = instruction =>
+  instruction.startsWith('nop')
+    ? instruction.replace('nop', 'jmp')
+    : instruction.replace('jmp', 'nop')
+
+// Execute the given array of instructions after having patched the one at given
+// index.
+// @param {String[]} instructions - List of instructions making the program
+// @param {Number} index - Index of instruction to patch
+// @return {Object} `accumulator` value and 0 or 1 `exit` code
+const runPatchedProgram = (instructions, index) =>
+  runProgram(
+    updateAtIndex(instructions, index, patchInstruction(instructions[index]))
+  )
+
+// Monkey-patch and execute the given set instructions to find the faulty line
+// and successfully return.
+// @param {String[]} instructions - List of instructions making the program
+// @return {Object} `accumulator` value and 0 or 1 `exit` code
+const runMonkeyPatchedProgram = instructions =>
+  instructions.reduce(
+    (output, instruction, index) =>
+      instruction.startsWith('acc') || output.exit === 0
+        ? output
+        : runPatchedProgram(instructions, index),
+    {}
+  )
+
+module.exports = { runProgram, runMonkeyPatchedProgram }
---
+++ src/day-8/test.js
@@ -0,0 +1,59 @@
+const test = require('ava')
+const { runProgram, runMonkeyPatchedProgram } = require('./')
+const instructions = require('../helpers/readInput')(__dirname)
+
+const buggyInput = `
+nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6
+`
+  .trim()
+  .split('\n')
+
+const fixedInput = `
+nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+nop -4
+acc +6
+`
+  .trim()
+  .split('\n')
+
+test('Day 8.1', t => {
+  const output = runProgram(buggyInput)
+  t.is(output.accumulator, 5)
+  t.is(output.exit, 1)
+})
+
+test('Day 8.2', t => {
+  const output = runProgram(fixedInput)
+  t.is(output.accumulator, 8)
+  t.is(output.exit, 0)
+
+  const fixedOutput = runMonkeyPatchedProgram(buggyInput)
+  t.is(fixedOutput.accumulator, 8)
+  t.is(fixedOutput.exit, 0)
+
+  t.deepEqual(output, fixedOutput)
+})
+
+test('Day 8 — Solutions', t => {
+  const output = runProgram(instructions)
+  t.is(output.accumulator, 1723)
+  t.is(output.exit, 1)
+
+  const fixedOutput = runMonkeyPatchedProgram(instructions)
+  t.is(fixedOutput.accumulator, 846)
+  t.is(fixedOutput.exit, 0)
+})
