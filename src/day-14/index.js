edc8f966a47acbed751739d8df714815c140fb08
Kitty Giraudel <hugo.giraudel@gmail.com>
2020-12-14 12:30:31 +0100

    Add day 14

---
+++ src/day-14/index.js
@@ -0,0 +1,87 @@
+const sum = require('../helpers/sum')
+const updateAtIndex = require('../helpers/updateAtIndex')
+
+const toBin = value => (value >>> 0).toString(2).padStart(36, '0')
+const toDec = value => parseInt(value, 2)
+const strMap = (value, cb) => value.split('').map(cb).join('')
+
+// Parse the given input into a comprehensible set of instructions.
+// @param {String} input - Input data
+// @return {*[]}
+const parseProgram = input =>
+  input.map(line =>
+    line.startsWith('mask')
+      ? line.replace('mask = ', '')
+      : line
+          .match(/mem\[(\d+)\] = (\d+)/)
+          .slice(1)
+          .map(Number)
+  )
+
+// Apply given mask to given value.
+// @param {Number} value - Value as a base-10 integers
+// @param {String} mask - Bitmask
+// @return {Number}
+const applyMask = (value, mask) =>
+  toDec(strMap(toBin(value), (char, i) => (mask[i] === 'X' ? char : mask[i])))
+
+// Loose processor: the mask is used to apply a value at given memory location.
+// @param {Object} memory - Memory object
+// @param {String} mask - Bitmask
+// @param {String|Number[]} instruction - Mask (string value) or memory (key,
+//                                        value pair of numbers) instruction
+// @return [memory, mask] Memory and mask for further processing
+const processLoose = ([memory, mask], instruction) => {
+  if (typeof instruction === 'string') return [memory, instruction]
+  memory[String(instruction[0])] = applyMask(instruction[1], mask)
+  return [memory, mask]
+}
+
+// Resolve wildcard characters (`X`) in given address to expand it into multiple
+// possible addresses.
+// @param {String[]} value - Masked value
+// @return {String[]}
+const resolveAddresses = value =>
+  value.includes('X')
+    ? [
+        ...resolveAddresses(updateAtIndex(value, value.indexOf('X'), '0')),
+        ...resolveAddresses(updateAtIndex(value, value.indexOf('X'), '1')),
+      ]
+    : [value]
+
+// Retrieve all possible addresses for the given value and mask.
+// @param {Number} value - Memory location
+// @param {String} mask - Bitmask
+// @return {Number[]}
+const getAddresses = (value, mask) =>
+  resolveAddresses(
+    strMap(toBin(value), (char, i) => (mask[i] !== '0' ? mask[i] : char))
+  )
+
+// Strict processor: the mask is used to retrieve possible addresses to write
+// the value at.
+// @param {Object} memory - Memory object
+// @param {String} mask - Bitmask
+// @param {String|Number[]} instruction - Mask (string value) or memory (key,
+//                                        value pair of numbers) instruction
+// @return [memory, mask] Memory and mask for further processing
+const processStrict = ([memory, mask], instruction) => {
+  if (typeof instruction === 'string') return [memory, instruction]
+  getAddresses(instruction[0], mask).forEach(
+    address => (memory[address] = instruction[1])
+  )
+  return [memory, mask]
+}
+
+// Execute the given program with the given processor.
+// @param {String[]} input - Raw unparsed program input
+// @param {Function} processor - Processor to treat every instruction
+// @return {Number}
+const executeProgram = (input, processor) => {
+  const program = parseProgram(input)
+  const [memory] = program.reduce(processor, [{}, null])
+
+  return sum(Object.values(memory))
+}
+
+module.exports = { parseProgram, executeProgram, processLoose, processStrict }
---
+++ src/day-14/test.js
@@ -0,0 +1,43 @@
+const test = require('ava')
+const {
+  parseProgram,
+  executeProgram,
+  processLoose,
+  processStrict,
+} = require('.')
+const input = require('../helpers/readInput')(__dirname)
+
+test('Day 14.1', t => {
+  const example = `
+  mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
+  mem[8] = 11
+  mem[7] = 101
+  mem[8] = 0
+  `
+    .trim()
+    .split('\n')
+  t.deepEqual(parseProgram(example), [
+    'XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X',
+    [8, 11],
+    [7, 101],
+    [8, 0],
+  ])
+  t.is(executeProgram(example, processLoose), 165)
+})
+
+test('Day 14.2', t => {
+  const example = `
+mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1
+`
+    .trim()
+    .split('\n')
+  t.is(executeProgram(example, processStrict), 208)
+})
+
+test('Day 14 — Solutions', t => {
+  t.is(executeProgram(input, processLoose), 15172047086292)
+  t.is(executeProgram(input, processStrict), 4197941339968)
+})
