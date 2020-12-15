2cd05610e300b887007097d1ec482f837de1cfe0
Kitty Giraudel <hugo.giraudel@gmail.com>
2020-12-15 12:56:22 +0100

    Add day 15

--- src/day-15/index.js
+++ src/day-15/index.js
@@ -1,14 +1,15 @@
-const isNew = (array, value) => array.indexOf(value) == array.lastIndexOf(value)
+// Get the last number said after `target` rounds with `numbers` as a starting
+// point.
+// @param {Number[]} numbers - Initial set of numbers
+// @param {Number} rounds - Rounds to play
+const get = (numbers, rounds = 1) => {
+  const map = new Map(numbers.map((n, i) => [n, i]))
+  let last = numbers[numbers.length - 1]
 
-const get = (numbers, target) => {
-  const history = numbers.slice(0)
-  const map = new Map(history.map((number, index) => [number, [index + 1]]))
-  let last = history[history.length - 1]
-
-  for (let i = history.length + 1; i <= target; i++) {
-    const peek = map.get(last)
-    last = peek.length === 1 ? 0 : peek[1] - peek[0]
-    map.set(last, (map.get(last) || []).slice(-1).concat(i))
+  for (let i = numbers.length; i < rounds; i++) {
+    const value = map.has(last) ? i - map.get(last) - 1 : 0
+    map.set(last, i - 1)
+    last = value
   }
 
   return last
