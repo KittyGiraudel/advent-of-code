69df9fcca88c84f45238e0e7dc9ffada18e66b24
Kitty Giraudel <kitty.giraudel@gmail.com>
2021-12-03 10:57:19 +0100

    Add day 03

---
+++ src/2021/day-03/test.js
@@ -0,0 +1,48 @@
+const test = require('ava')
+const { getEpsilonAndGamma, getOxygen, getCO2 } = require('./')
+const items = require('../../helpers/readInput')(__dirname)
+
+const sample = `00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010`.split('\n')
+
+test('Day 3.1', t => {
+  const gamma = getEpsilonAndGamma(sample).gamma
+  const epsilon = getEpsilonAndGamma(sample).epsilon
+  const power = parseInt(gamma, 2) * parseInt(epsilon, 2)
+
+  t.is(gamma, '10110')
+  t.is(epsilon, '01001')
+  t.is(power, 198)
+})
+
+test('Day 3.2', t => {
+  const oxygen = getOxygen(sample)
+  const CO2 = getCO2(sample)
+  const life = parseInt(oxygen, 2) * parseInt(CO2, 2)
+
+  t.is(oxygen, '10111')
+  t.is(CO2, '01010')
+  t.is(life, 230)
+})
+
+test('Day 3 — Solutions', t => {
+  const gamma = getEpsilonAndGamma(items).gamma
+  const epsilon = getEpsilonAndGamma(items).epsilon
+  const power = parseInt(gamma, 2) * parseInt(epsilon, 2)
+  t.is(power, 4103154)
+
+  const oxygen = getOxygen(items)
+  const CO2 = getCO2(items)
+  const life = parseInt(oxygen, 2) * parseInt(CO2, 2)
+  t.is(life, 4245351)
+})
