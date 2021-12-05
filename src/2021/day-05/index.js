17bef4f8fc2a84ee47c62da94467f9ec62fa1269
Kitty Giraudel <kitty.giraudel@gmail.com>
2021-12-05 12:24:29 +0100

    Add day 05

---
+++ src/2021/day-05/index.js
@@ -0,0 +1,51 @@
+const getOverlappingPoints = (lines, withDiagonals = false) => {
+  // Break down every line into a pair of vectors, each vector being a pair of
+  // number (x,y coordinates).
+  const vectors = lines.map(line =>
+    line.split(' -> ').map(point => point.split(',').map(Number))
+  )
+
+  const map = vectors.reduce((acc, vector) => {
+    const [start, end] = vector
+    const bump = (x, y) => acc.set(`${x},${y}`, (acc.get(`${x},${y}`) || 0) + 1)
+    const min = Math.min.bind(Math)
+    const max = Math.max.bind(Math)
+    const abs = Math.abs.bind(Math)
+
+    // Handle vertical lines
+    if (start[0] === end[0]) {
+      for (let y = min(start[1], end[1]); y <= max(start[1], end[1]); y++)
+        bump(start[0], y)
+    }
+
+    // Handle horizonal lines
+    else if (start[1] === end[1]) {
+      for (let x = min(start[0], end[0]); x <= max(start[0], end[0]); x++)
+        bump(x, start[1])
+    }
+
+    // Handle diagonal lines
+    else if (
+      withDiagonals &&
+      (start[0] + start[1] === end[0] + end[1] ||
+        abs(start[0] - start[1]) === abs(end[0] - end[1]))
+    ) {
+      for (let x = min(start[0], end[0]); x <= max(start[0], end[0]); x++) {
+        for (let y = min(start[1], end[1]); y <= max(start[1], end[1]); y++) {
+          if (
+            x + y === start[0] + start[1] ||
+            x - y === start[0] - start[1] ||
+            y - x === start[1] - start[0]
+          )
+            bump(x, y)
+        }
+      }
+    }
+
+    return acc
+  }, new Map())
+
+  return Array.from(map.values()).filter(value => value > 1).length
+}
+
+module.exports = { getOverlappingPoints }
---
+++ src/2021/day-05/test.js
@@ -0,0 +1,27 @@
+const test = require('ava')
+const { getOverlappingPoints } = require('./')
+const input = require('../../helpers/readInput')(__dirname)
+
+const sample = `0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2`.split('\n')
+
+test('Day 5.1', t => {
+  t.is(getOverlappingPoints(sample), 5)
+})
+
+test('Day 5.2', t => {
+  t.is(getOverlappingPoints(sample, true), 12)
+})
+
+test('Day 5 — Solutions', t => {
+  t.is(getOverlappingPoints(input), 7269)
+  t.is(getOverlappingPoints(input, true), 21140)
+})
