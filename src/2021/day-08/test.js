e5215cc36b69c2e14abdd2fe967a317c89de657e
Kitty Giraudel <kitty.giraudel@gmail.com>
2021-12-08 10:50:38 +0100

    Add day 08

---
+++ src/2021/day-08/test.js
@@ -0,0 +1,96 @@
+const test = require('ava')
+const { count, decode, total } = require('./')
+const input = require('../../helpers/readInput')(__dirname)
+
+const sample =
+  `be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce`.split(
+    '\n'
+  )
+
+test('Day 8.1', t => {
+  t.is(count(sample), 26)
+})
+
+test('Day 8.2', t => {
+  t.is(
+    decode(
+      'acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf'
+    ),
+    '5353'
+  )
+  t.is(
+    decode(
+      'be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe'
+    ),
+    '8394'
+  )
+  t.is(
+    decode(
+      'edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc'
+    ),
+    '9781'
+  )
+  t.is(
+    decode(
+      'fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg'
+    ),
+    '1197'
+  )
+  t.is(
+    decode(
+      'fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb'
+    ),
+    '9361'
+  )
+  t.is(
+    decode(
+      'aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea'
+    ),
+    '4873'
+  )
+  t.is(
+    decode(
+      'fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb'
+    ),
+    '8418'
+  )
+  t.is(
+    decode(
+      'dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe'
+    ),
+    '4548'
+  )
+  t.is(
+    decode(
+      'bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef'
+    ),
+    '1625'
+  )
+  t.is(
+    decode(
+      'egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb'
+    ),
+    '8717'
+  )
+  t.is(
+    decode(
+      'gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce'
+    ),
+    '4315'
+  )
+  t.is(total(sample), 61229)
+})
+
+test('Day 8 — Solutions', t => {
+  t.is(count(input), 554)
+  t.is(total(input), 990964)
+})
