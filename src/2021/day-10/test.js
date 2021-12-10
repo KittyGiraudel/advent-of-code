6905755fcad9186117ccc2e2ed551fb5ff5cd219
Kitty Giraudel <kitty.giraudel@gmail.com>
2021-12-10 10:33:55 +0100

    Add day 10

---
+++ src/2021/day-10/test.js
@@ -0,0 +1,36 @@
+const test = require('ava')
+const { processLine, getCorruptionScore, getCompletionScore } = require('./')
+const input = require('../../helpers/readInput')(__dirname)
+
+const sample = `[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]`.split('\n')
+
+test('Day 10.1', t => {
+  t.is(processLine('(]').type, 'CORRUPTED')
+  t.is(processLine('{()()()>').type, 'CORRUPTED')
+  t.is(processLine('(((()))}').type, 'CORRUPTED')
+  t.is(processLine('<([]){()}[{}])').type, 'CORRUPTED')
+  t.is(processLine('{([(<{}[<>[]}>{[]{[(<()>').type, 'CORRUPTED')
+  t.is(processLine('[[<[([]))<([[{}[[()]]]').type, 'CORRUPTED')
+  t.is(processLine('[{[{({}]{}}([{[{{{}}([]').type, 'CORRUPTED')
+  t.is(processLine('[<(<(<(<{}))><([]([]()').type, 'CORRUPTED')
+  t.is(processLine('<{([([[(<>()){}]>(<<{{').type, 'CORRUPTED')
+  t.is(getCorruptionScore(sample), 26397)
+})
+
+test('Day 10.2', t => {
+  t.is(getCompletionScore(sample), 288957)
+})
+
+test('Day 10 — Solutions', t => {
+  t.is(getCorruptionScore(input), 243939)
+  t.is(getCompletionScore(input), 2421222841)
+})
