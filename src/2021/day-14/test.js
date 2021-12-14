8225cd343bb89abdcd291c541f8415fa069e6c8b
Kitty Giraudel <kitty.giraudel@gmail.com>
2021-12-14 17:15:34 +0100

    Add day 14

---
+++ src/2021/day-14/test.js
@@ -0,0 +1,35 @@
+const test = require('ava')
+const { count } = require('./')
+const input = require('../../helpers/readInput')(__dirname, '\n\n')
+
+const sample = `NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C`.split('\n\n')
+
+test('Day 14.1', t => {
+  t.is(count(sample, 10), 1588)
+})
+
+test('Day 14.2', t => {
+  t.is(count(sample, 40), 2188189693529)
+})
+
+test('Day 14 — Solutions', t => {
+  t.is(count(input, 10), 2321)
+  t.is(count(input, 40), 2399822193707)
+})
