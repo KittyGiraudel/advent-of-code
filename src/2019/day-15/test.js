ca2fac579304354b6a5d1004bb187f3515346a8a
Kitty Giraudel <kitty.giraudel@gmail.com>
2021-12-30 12:17:58 +0100

    Remove incomplete days

--- src/2019/day-15/test.js
+++
@@ -1,7 +0,0 @@
-const test = require('ava')
-const {} = require('./')
-const [input] = require('../../helpers/readInput')(__dirname)
-
-test.skip('Day 15.1', t => {})
-test.skip('Day 15.2', t => {})
-test.skip('Day 15 — Solutions', t => {})
--- src/2021/day-19/index.js
+++
@@ -1,4 +0,0 @@
-const parseInput = scanners =>
-  scanners.slice(1).map(line => line.split(',').map(Number))
-
-module.exports = {}
