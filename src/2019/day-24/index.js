1cfa610598c60c8fcda2489141b48f08bc370533
Kitty Giraudel <kitty.giraudel@gmail.com>
2022-01-08 23:04:33 +0100

    Add day 24

---
+++ src/2019/day-24/index.js
@@ -0,0 +1,38 @@
+const $ = require('../../helpers')
+
+const cycle = curr => {
+  let next = ''
+  let width = Math.sqrt(curr.length)
+
+  for (let i = 0; i < curr.length; i++) {
+    const bugs = [
+      i - width,
+      i % width === width - 1 ? null : i + 1,
+      i + width,
+      i % width === 0 ? null : i - 1,
+    ].filter(i => curr?.[i] === '#').length
+    next += (curr[i] === '#' ? bugs === 1 : [1, 2].includes(bugs)) ? '#' : '.'
+  }
+
+  return next
+}
+
+const findBiodiversity = input => {
+  const history = []
+  let curr = input.join('')
+
+  while (!history.includes(curr)) {
+    history.push(curr)
+    curr = cycle(curr)
+  }
+
+  return $.sum(
+    curr
+      .split('')
+      .map((v, i) => (v === '#' ? i : null))
+      .filter(value => typeof value === 'number')
+      .map(i => Math.pow(2, i))
+  )
+}
+
+module.exports = { cycle, findBiodiversity }
