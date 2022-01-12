5ab92f0e948f53de9da90d260884b9f40f1829ae
Kitty Giraudel <kitty.giraudel@gmail.com>
2022-01-12 14:19:02 +0100

    Add day 08

---
+++ src/2018/day-08/test.js
@@ -0,0 +1,18 @@
+const test = require('ava')
+const { score, parse } = require('./')
+const input = require('../../helpers/readInput')(__dirname, ' ').map(Number)
+
+const sample = `2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2`.split(' ').map(Number)
+
+test('Day 8.1', t => {
+  t.is(score(parse(sample.slice(0))), 138)
+})
+
+test('Day 8.2', t => {
+  t.is(parse(sample.slice(0), true).value, 66)
+})
+
+test('Day 8 — Solutions', t => {
+  t.is(score(parse(input.slice(0))), 38780)
+  t.is(parse(input.slice(0), true).value, 18232)
+})
