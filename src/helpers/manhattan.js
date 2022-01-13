0dcc35ae0807bf6029618a0ec868753e304168a4
Kitty Giraudel <kitty.giraudel@gmail.com>
2022-01-13 22:26:16 +0100

    Add day 13

--- src/helpers/manhattan.js
+++ src/helpers/manhattan.js
@@ -1,3 +1,10 @@
-const manhattan = (a, b) => Math.abs(b.x - a.x) + Math.abs(b.y - a.y)
+const manhattan = (a, b) => {
+  const aX = Array.isArray(a) ? a[0] : a.x
+  const aY = Array.isArray(a) ? a[1] : a.y
+  const bX = Array.isArray(b) ? b[0] : b.x
+  const bY = Array.isArray(b) ? b[1] : b.y
+
+  return Math.abs(bX - aX) + Math.abs(bY - aY)
+}
 
 module.exports = manhattan
