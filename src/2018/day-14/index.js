da207373bb6d1837f7a8ca5493ecfd4258ca9f8f
Kitty Giraudel <kitty.giraudel@gmail.com>
2022-01-14 13:19:45 +0100

    Add day 14

---
+++ src/2018/day-14/index.js
@@ -0,0 +1,53 @@
+const $ = require('../../helpers')
+
+// I also tried a version using a string as the board, but it turns out that
+// manipulating long strings is way slower than manipulating large arrays in V8
+// for some reason.
+const play = (board, players) => {
+  const recipes = players.map(index => board[index])
+  const sum = recipes[0] + recipes[1]
+  // Interesting piece of trivia: this version which I originally started with
+  // is orders of magnitude slower than the math-oriented one. Which makes sense
+  // since it does multiple type casting (number to string, then array from
+  // string, then string to number on the 1,2 items).
+  // const sum = Array.from(String($.sum(recipes))).map(Number)
+  if (sum >= 10) board.push(Math.floor(sum / 10))
+  board.push(sum % 10)
+  // Storing the length of the array to avoid a second peek per play, which
+  // might maybe matter at extreme lengths?
+  const length = board.length
+  players[0] = (players[0] + 1 + recipes[0]) % length
+  players[1] = (players[1] + 1 + recipes[1]) % length
+}
+
+const cook1 = count => {
+  const board = [3, 7]
+  const players = [0, 1]
+
+  while (board.length < count + 10) play(board, players)
+
+  return board.slice(count, count + 10).join('')
+}
+
+const cook2 = (needle, upper) => {
+  const board = [3, 7]
+  const players = [0, 1]
+  let i = 0
+
+  // Interestingly, it’s the check that’s costly, and not the moves per se,
+  // which is why iterating an arbitrary high number of times is significantly
+  // faster than iterating only the right number of times, as reported by this
+  // Reddit comment:
+  // https://www.reddit.com/r/adventofcode/comments/a61ojp/comment/ebsidve/
+  // I tried playing with various conditions to see if one of them would yield
+  // fast results, but no dice:
+  //    !board.join('').endsWith(needle)
+  //    board.join('').indexOf(needle) !== -1
+  //    board.join('').search(needle) !== -1
+  while (upper ? i++ < upper : !board.join('').endsWith(needle))
+    play(board, players)
+
+  return upper ? board.join('').search(needle) : board.length - needle.length
+}
+
+module.exports = { cook1, cook2 }
---
+++ src/2018/day-14/test.js
@@ -0,0 +1,21 @@
+const test = require('ava')
+const { cook1, cook2 } = require('./')
+
+test('Day 14.1', t => {
+  t.is(cook1(9), '5158916779')
+  t.is(cook1(5), '0124515891')
+  t.is(cook1(18), '9251071085')
+  t.is(cook1(2018), '5941429882')
+})
+
+test('Day 14.2', t => {
+  t.is(cook2('51589'), 9)
+  t.is(cook2('01245'), 5)
+  t.is(cook2('92510'), 18)
+  t.is(cook2('59414'), 2018)
+})
+
+test('Day 14 — Solutions', t => {
+  t.is(cook1(260321), '9276422810')
+  t.is(cook2('260321', 16_000_000), 20319117)
+})
