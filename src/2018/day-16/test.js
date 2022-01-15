de9d599c8e94bae12299b8d4b8ddb15cd3626480
Kitty Giraudel <kitty.giraudel@gmail.com>
2022-01-15 14:13:36 +0100

    Add day 16

---
+++ src/2018/day-16/test.js
@@ -0,0 +1,12 @@
+const test = require('ava')
+const { debug } = require('./')
+const input = require('../../helpers/readInput')(__dirname, '\n\n')
+
+test.skip('Day 16.1', t => {})
+
+test.skip('Day 16.2', t => {})
+
+test('Day 16 — Solutions', t => {
+  //t.is(debug(input), 588)
+  t.is(debug(input)[0], 627)
+})
