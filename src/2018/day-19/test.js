99526a204ddb9eaa27bb341b219fea4946b93cad
Kitty Giraudel <kitty.giraudel@gmail.com>
2022-01-16 14:49:46 +0100

    Add day 19

---
+++ src/2018/day-19/test.js
@@ -0,0 +1,23 @@
+const test = require('ava')
+const { run } = require('./')
+const input = require('../../helpers/readInput')(__dirname)
+
+const sample = `#ip 0
+seti 5 0 1
+seti 6 0 2
+addi 0 1 0
+addr 1 2 3
+setr 1 0 0
+seti 8 0 4
+seti 9 0 5`.split('\n')
+
+test('Day 19.1', t => {
+  t.is(run(sample), 6)
+})
+
+test.skip('Day 19.2', t => {})
+
+test('Day 19 — Solutions', t => {
+  t.is(run(input, 0), 1536)
+  //t.is(run(input, 1), 1536)
+})
