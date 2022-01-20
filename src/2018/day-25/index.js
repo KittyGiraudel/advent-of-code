d4b2d1e98957d3c8414ac4c43c78f05a6439a2e8
Kitty Giraudel <kitty.giraudel@gmail.com>
2022-01-20 19:06:28 +0100

    Add day 25

---
+++ src/2018/day-25/index.js
@@ -0,0 +1,49 @@
+const $ = require('../../helpers')
+
+// Interestingly enough, this is almost the exact same algorithm as outlined by
+// this Reddit post (who scored #10/#58), so I’m pretty happy:
+// https://www.reddit.com/r/adventofcode/comments/a9c61w/comment/eci5rgz/?utm_source=reddit&utm_medium=web2x&context=3
+const observe = input => {
+  const lines = input.map($.toCoords)
+  const constellations = []
+  const isInConstellation = (constellation, coords) =>
+    Array.from(constellation).some(point => $.manhattan(point, coords) <= 3)
+
+  lines.forEach(coords => {
+    // For every given point, list all constellations it belongs to (called
+    // “hosts” here).
+    const hosts = constellations
+      .map((constellation, index) =>
+        isInConstellation(constellation, coords) ? index : null
+      )
+      .filter(index => index !== null)
+
+    // If the point doesn’t belong to any constellation, it should start one of
+    // its own.
+    if (hosts.length === 0) {
+      constellations.push(new Set([coords]))
+    }
+
+    // Otherwise, it should do 2 things:
+    // 1. Be added to the first host it belongs to.
+    // 2. All the other hosts should be merged into the first and deleted, since
+    //    that point served as a bridge to bring merge these constellations.
+    else {
+      const first = hosts.shift()
+
+      constellations[first].add(coords)
+
+      hosts.reverse().forEach(cIndex => {
+        Array.from(constellations[cIndex]).forEach(coords =>
+          constellations[first].add(coords)
+        )
+
+        constellations.splice(cIndex, 1)
+      })
+    }
+  })
+
+  return constellations.length
+}
+
+module.exports = { observe }
