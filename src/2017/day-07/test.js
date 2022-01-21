fc2f4b04fa27b12671336288c4c3b8ced31d5062
Kitty Giraudel <kitty.giraudel@gmail.com>
2022-01-21 17:00:01 +0100

    Add day 07

---
+++ src/2017/day-07/test.js
@@ -0,0 +1,30 @@
+const test = require('ava')
+const { run } = require('./')
+const input = require('../../helpers/readInput')(__dirname)
+
+const sample = `pbga (66)
+xhth (57)
+ebii (61)
+havc (66)
+ktlj (57)
+fwft (72) -> ktlj, cntj, xhth
+qoyq (66)
+padx (45) -> pbga, havc, qoyq
+tknk (41) -> ugml, padx, fwft
+jptl (61)
+ugml (68) -> gyxo, ebii, jptl
+gyxo (61)
+cntj (57)`.split('\n')
+
+test('Day 7.1', t => {
+  t.is(run(sample).root, 'tknk')
+})
+
+test('Day 7.2', t => {
+  t.is(run(sample).fix, 60)
+})
+
+test('Day 7 — Solutions', t => {
+  t.is(run(input).root, 'vtzay')
+  t.is(run(input).fix, 910)
+})
