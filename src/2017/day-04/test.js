0c9e7f0ec764c21cdad469b199b717126cb3e094
Kitty Giraudel <kitty.giraudel@gmail.com>
2022-01-21 15:04:50 +0100

    Add day 04

---
+++ src/2017/day-04/test.js
@@ -0,0 +1,22 @@
+const test = require('ava')
+const { validate, validateStrict } = require('./')
+const input = require('../../helpers/readInput')(__dirname)
+
+test('Day 4.1', t => {
+  t.is(validate('aa bb cc dd ee'), true)
+  t.is(validate('aa bb cc dd aa'), false)
+  t.is(validate('aa bb cc dd aaa'), true)
+})
+
+test('Day 4.2', t => {
+  t.is(validateStrict('abcde fghij'), true)
+  t.is(validateStrict('abcde xyz ecdab'), false)
+  t.is(validateStrict('a ab abc abd abf abj'), true)
+  t.is(validateStrict('iiii oiii ooii oooi oooo'), true)
+  t.is(validateStrict('oiii ioii iioi iiio'), false)
+})
+
+test('Day 4 — Solutions', t => {
+  t.is(input.filter(validate).length, 383)
+  t.is(input.filter(validateStrict).length, 265)
+})
