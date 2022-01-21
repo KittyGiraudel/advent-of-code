76eb6af0e3fa5912aa5f0f5f3b2e75ca084e86b0
Kitty Giraudel <kitty.giraudel@gmail.com>
2022-01-21 15:17:29 +0100

    Add day 05

---
+++ src/2017/day-05/index.js
@@ -0,0 +1,18 @@
+const $ = require('../../helpers')
+
+const run = (memory, boundary = Infinity) => {
+  memory = memory.slice(0)
+  let index = 0
+  let i = 0
+
+  while (typeof memory[index] !== 'undefined') {
+    i++
+    const offset = memory[index]
+    memory[index] = memory[index] + (offset >= boundary ? -1 : +1)
+    index += offset
+  }
+
+  return i
+}
+
+module.exports = { run }
