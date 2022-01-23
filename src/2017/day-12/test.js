97657c88d665d3500f5cf6fb610b2e1c07cb5959
Kitty Giraudel <kitty.giraudel@gmail.com>
2022-01-23 10:17:52 +0100

    Add day 12

---
+++ src/2017/day-12/test.js
@@ -0,0 +1,23 @@
+const test = require('ava')
+const { run } = require('./')
+const input = require('../../helpers/readInput')(__dirname)
+
+const sample = `0 <-> 2
+1 <-> 1
+2 <-> 0, 3, 4
+3 <-> 2, 4
+4 <-> 2, 3, 6
+5 <-> 6
+6 <-> 4, 5`.split('\n')
+
+test('Day 12.1', t => {
+  t.is(run(sample)[0], 6)
+})
+
+test('Day 12.2', t => {
+  t.is(run(sample)[1], 2)
+})
+
+test('Day 12 — Solutions', t => {
+  t.deepEqual(run(input), [141, 171])
+})
