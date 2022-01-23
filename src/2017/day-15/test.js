40d64190f9e8563bc561d96791930624ea190a68
Kitty Giraudel <kitty.giraudel@gmail.com>
2022-01-23 13:59:30 +0100

    Add day 15

---
+++ src/2017/day-15/test.js
@@ -0,0 +1,17 @@
+const test = require('ava')
+const { run } = require('./')
+
+test('Day 15.1', t => {
+  t.is(run(65, 8921, 5), 1)
+  t.is(run(65, 8921, 40_000_000), 588)
+})
+
+test('Day 15.2', t => {
+  t.is(run(65, 8921, 1056, 4, 8), 1)
+  t.is(run(65, 8921, 5_000_000, 4, 8), 309)
+})
+
+test('Day 15 — Solutions', t => {
+  t.is(run(277, 349, 40_000_000), 592)
+  t.is(run(277, 349, 5_000_000, 4, 8), 320)
+})
