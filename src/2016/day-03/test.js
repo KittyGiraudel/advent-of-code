1b91edfae863c8f14ea981b47914513908c5582f
Kitty Giraudel <kitty.giraudel@gmail.com>
2022-01-25 14:59:45 +0100

    Add day 03

---
+++ src/2016/day-03/test.js
@@ -0,0 +1,11 @@
+const test = require('ava')
+const { run } = require('./')
+const input = require('../../helpers/readInput')(__dirname)
+
+test.skip('Day 3.1', t => {})
+
+test.skip('Day 3.2', t => {})
+
+test('Day 3 — Solutions', t => {
+  t.deepEqual(run(input), [862, 1577])
+})
