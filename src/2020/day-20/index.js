9c1225dc772c4b3d0092b78794a33dda1b853d3c
Kitty Giraudel <kitty.giraudel@gmail.com>
2022-01-27 13:14:13 +0100

    Rewrite day 20

--- src/2020/day-20/index.js
+++ src/2020/day-20/index.js
@@ -1,235 +1,166 @@
 const $ = require('../../helpers')
 
-const getTop = image => image[0]
-const getBottom = image => image[image.length - 1]
-const getLeft = image => image.map(row => row[0]).join('')
-const getRight = image => image.map(row => row[row.length - 1]).join('')
+// Terminology for this whole file:
+// Snapshot:
+//    A raw image block from the input file, starting with its header line
+//    containing its ID.
+// Tile:
+//    A parsed snapshot with an ID, a grid, and 4 sides.
+// Mozaic:
+//    The square grid of tiles in the right orientation, yielded by solving
+//    the jigsaw puzzle.
+// Image:
+//    The reassembled mozaic once the puzzle is complete (made by shaving off
+//    the sides of every tile’s grid and putting them all together).
+// Cell:
+//    A single character value from a snapshot (either `#` or `.`).
 
-// Read the variant of a tile at its record variant index.
-// @param {Tile} tile - Tile to read the variant from
-// @return {Image}
-const readTile = tile => tile.variants[tile.actualVariantIndex]
-
-// Check whether the right tile can fit next to the left tile.
-// @param {Tile} left - Left tile (if any)
-// @param {Tile} right - Right tile
-// @return {Boolean}
-const checkLeft = (left, right) =>
-  !left || getRight(readTile(left)) === getLeft(readTile(right))
-
-// Check whether the bottom tile can fit next to the top tile.
-// @param {Tile} top - Top tile (if any)
-// @param {Tile} bottom - Bottom tile
-// @return {Boolean}
-const checkTop = (top, bottom) =>
-  !top || getTop(readTile(bottom)) === getBottom(readTile(top))
-
-// Taken and reworked from:
-// https://github.com/bylexus/adventofcode2020/blob/95998d0a55e17091f0d03ef108407a3d151a182f/20-jurassic-jigsaw.py#L98
-const checkTile = (tiles, used, matrix, row, col, size) => {
-  if (row >= size || col >= size) return true
-
-  for (let tile of tiles) {
-    if (used.has(tile.id)) continue
-
-    used.add(tile.id)
-    matrix[row][col] = tile
-
-    for (let i = 0; i < tile.variants.length; i++) {
-      tile.actualVariantIndex = i
-
-      const leftFit = col > 0 ? checkLeft(matrix[row][col - 1], tile) : true
-      const topFit = row > 0 ? checkTop(matrix[row - 1][col], tile) : true
-
-      if (leftFit && topFit) {
-        const nextCol = (col + 1) % size
-        const nextRow = Math.floor((size * row + col + 1) / size)
-
-        if (checkTile(tiles, used, matrix, nextRow, nextCol, size)) {
-          tile.matchingVariantIndex = i
-          return true
-        }
-      }
-
-      tile.actualVariantIndex = null
-    }
-
-    matrix[row][col] = undefined
-    used.delete(tile.id)
-  }
-
-  return false
-}
-
-const rotateMatrix = rows => {
-  const cols = Array.isArray(rows[0]) ? rows[0] : rows[0].split('')
-
-  return cols.map((_, col) =>
-    rows
-      .map(row => row[col])
-      .reverse()
-      .join('')
-  )
-}
-
-// Flip a matrix along the vertical axis.
-// @param {Matrix} matrix - Matrix to flip
-// @return {Matrix}
-const flip = matrix => {
-  const out = $.grid.init(matrix.length)
-
-  for (let row = 0; row < out.length; row++) {
-    for (let col = 0; col < out.length; col++) {
-      out[row][out.length - 1 - col] = matrix[row][col]
-    }
-  }
-
-  return out.map(row => row.join(''))
-}
-
-// Rotate a matrix clockwise.
-// @param {Matrix} matrix - Matrix to rotate
-// @return {Matrix}
-const rotate = (matrix, times = 1) => {
-  for (let i = 0; i < times; i++) matrix = rotateMatrix(matrix)
-  return matrix
-}
-
-// Compute the 8 variants (flipped and rotated) of an image.
-// @param {Matrix} image - Image to compute the variants for
-// @return {Matrix[]}
-const getVariants = image =>
-  [
-    i => rotate(i, 4),
-    i => rotate(i, 1),
-    i => rotate(i, 2),
-    i => rotate(i, 3),
-    i => flip(i),
-    i => rotate(flip(i), 1),
-    i => rotate(flip(i), 2),
-    i => rotate(flip(i), 3),
-  ].map(fn => fn(image))
-
-// Parse an image to extract its ID and its 8 variants (flipped and rotated).
-// @param {String} input - Raw tile input
-// @return {Object}
-const parseImage = input => {
-  const [header, ...rows] = input.split('\n')
-  const id = +header.match(/Tile (\d+):/)[1]
-
-  return { id, variants: getVariants(rows) }
-}
-
-const recompose = input => {
-  const matrix = completeJigsaw(input)
-  const width = matrix.length * (matrix[0][0].variants[0].length - 2)
-  const image = $.grid.init(width)
-
-  for (let ri = 0; ri < matrix.length; ri++) {
-    const row = matrix[ri]
-
-    for (let ci = 0; ci < row.length; ci++) {
-      const { variants, matchingVariantIndex } = row[ci]
-      const pixels = variants[matchingVariantIndex]
-      const plen = pixels.length - 2
-
-      for (let pri = 1; pri < pixels.length - 1; pri++) {
-        for (let pci = 1; pci < pixels.length - 1; pci++) {
-          image[ri * plen + pri - 1][ci * plen + pci - 1] = pixels[pri][pci]
-        }
-      }
-    }
-  }
-
-  return image
-}
-
-// Get the relative coords of all the cells composing a monster from the tip of
-// its head as the base cell.
-// @param {Number} row - Row (Y)
-// @param {Number} col - Col (X)
-// @return {Number[][]}
-const getMonsterShapeCoords = (row, col) => [
-  [row, col],
-  [row + 1, col],
-  [row + 1, col + 1],
-  [row + 1, col - 1],
-  [row + 2, col - 2],
-  [row + 2, col - 5],
-  [row + 1, col - 6],
-  [row + 1, col - 7],
-  [row + 2, col - 8],
-  [row + 2, col - 11],
-  [row + 1, col - 12],
-  [row + 1, col - 13],
-  [row + 2, col - 14],
-  [row + 2, col - 17],
-  [row + 1, col - 18],
+// The pattern to find in the image for part 2. It’s just an array of vectors so
+// it doesn’t *have* to be formatted like this, but I feel like it helps
+// visualizing so why not.
+// prettier-ignore
+const MONSTER_PATTERN = [
+                                                                 [-1,18],
+[0,0],        [0,5],[0,6],         [0,11],[0,12],          [0,17],[0,18],[0,19],
+    [1,1],[1,4],        [1,7],[1,10],          [1,13],[1,16],
 ]
 
-// Count all the sea cells (`#`).
-// @param {Matrix} image - Image to count waves in
-// @return {Number}
-const countSeaCells = image =>
-  image
-    .map(a => a.join(''))
-    .join('\n')
-    .match(/#/g)?.length ?? 0
+// Get every side of the grid, ordered top -> right -> bottom -> left (which
+// matters later on).
+const getSides = grid =>
+  [
+    /* Top    */ grid[0],
+    /* Right  */ $.column(grid, grid.length - 1),
+    /* Bottom */ $.peek(grid),
+    /* Left   */ $.column(grid, 0),
+  ].map(line => line.join(''))
 
-// Try to find monsters in every variant of the given image. In the one that has
-// monsters, count the remaining amount of sea cells.
-// @param {String[]} rawTiles - Raw unparsed tiles
-// @return {Number}
-const findMonsters = rawTiles => {
-  const image = recompose(rawTiles)
-  const variants = getVariants(image)
-  const makeArray = image => image.map(row => row.split(''))
+// Parse the snapshot into a collection of 8 variants (all rotations + flips
+// possibilities).
+const parseSnapshot = snapshot => {
+  const [header, ...lines] = snapshot.split('\n')
+  const [id] = header.match(/\d+/g)
+  const grid = $.grid.create(lines)
 
-  return Math.min(
-    ...variants.map(makeArray).map(image => {
-      image.forEach((line, row) =>
-        line.forEach((_, col) => {
-          const shape = getMonsterShapeCoords(row, col)
+  return $.grid.variants(grid).map(grid => ({
+    sides: getSides(grid),
+    grid,
+    id,
+  }))
+}
 
-          if (shape.every(coords => $.access(image, coords) === '#')) {
-            shape.forEach(coord => (image[coord[0]][coord[1]] = 'X'))
-          }
-        })
+// Brute-force the jigsaw in reading order (left-to-right and top-to-bottom)
+// until completion or dead-end starting with the given tile in the top-left
+// corner.
+const jigsaw = (tiles, start) => {
+  // The mozaic is guaranteed to be a square, and its length is the square root
+  // of the amount of tiles divided by 8 (since there are 8 variants per
+  // snapshot). For instance, if there are 72 tiles, it means 9 snapshots, so a
+  // 3x3 mozaic.
+  const length = Math.sqrt(tiles.length / 8)
+  const mozaic = $.grid.init(length, length)
+  const used = new Set()
+
+  mozaic[0][0] = start
+  used.add(start.id)
+
+  for (let ri = 0; ri < mozaic.length; ri++) {
+    for (let ci = 0; ci < mozaic[0].length; ci++) {
+      if (ri === 0 && ci === 0) continue
+
+      const left = $.access(mozaic, [ri, ci - 1])
+      const top = $.access(mozaic, [ri - 1, ci])
+
+      // Look amongst the tiles that are not used yet for one that matches the
+      // top and left tiles (if any).
+      const next = tiles.find(
+        ({ id, sides }) =>
+          !used.has(id) &&
+          (!left || left.sides[1] === sides[3]) &&
+          (!top || top.sides[2] === sides[0])
       )
 
-      return countSeaCells(image)
-    })
+      // If we could not find a tile here, that means previous pieces were
+      // incorrect and we can abort.
+      if (!next) return
+
+      mozaic[ri][ci] = next
+      used.add(next.id)
+    }
+  }
+
+  return mozaic
+}
+
+const assemble = mozaic => {
+  const grid = []
+
+  // For every row of 3 tiles …
+  for (let ri = 0; ri < mozaic.length; ri++) {
+    const row = mozaic[ri]
+
+    // Loop over the lines of the tiles, omitting the first and the last.
+    for (let i = 1; i < row[0].grid.length - 1; i++) {
+      grid.push(row.map(tile => tile.grid[i].slice(1, -1)).flat())
+    }
+  }
+
+  return grid
+}
+
+const isMonsterTail = (image, ri, ci) =>
+  MONSTER_PATTERN.every(vector => {
+    const coords = $.applyVector([ri, ci], vector)
+    return $.access(image, coords) === '#'
+  })
+
+// Count the amount of sea monsters in the given image.
+const countMonsters = image =>
+  $.grid.reduce(
+    image,
+    (acc, _, ri, ci) => acc + isMonsterTail(image, ri, ci),
+    0
   )
+
+// Iterate over all 8 variants of the given image (rotated and flipped) to find
+// the maximum number of sea monsters that can be spotted. Then, computed how
+// many `#` do not belong to a sea monster pattern.
+const inspectWaters = image => {
+  const variants = $.grid.variants(image)
+  const monsters = Math.max(...variants.map(countMonsters))
+  const sharps = $.countInString(image.flat().join(''), '#')
+
+  return sharps - monsters * MONSTER_PATTERN.length
 }
 
-// Complete the jigsaw puzzle by finding the proper order and orientation of
-// all tiles.
-// @param {String[]} rawTiles - Raw unparsed tiles
-// @return {Object[]}
-const completeJigsaw = rawTiles => {
-  const size = Math.sqrt(rawTiles.length)
-  const matrix = $.grid.init(size)
+// Compute the mozaic checksum by multiplying the ID of the 4 corner tiles.
+const checksum = mozaic => {
+  const length = mozaic.length
+  const corners = [
+    /* Top left     */ [0, 0],
+    /* Top right    */ [0, length - 1],
+    /* Bottom left  */ [length - 1, 0],
+    /* Bottom right */ [length - 1, length - 1],
+  ]
+    .map(coords => $.access(mozaic, coords))
+    .map(tile => tile.id)
 
-  checkTile(rawTiles.map(parseImage), new Set(), matrix, 0, 0, size)
-
-  return matrix
+  return $.product(corners)
 }
 
-// Get the full image ID by multiplying the ID of the 4 corner tiles.
-// @param {String[]} rawTiles - Raw unparsed tiles
-// @return {Number}
-const getFullImageId = rawTiles => {
-  const size = Math.sqrt(rawTiles.length)
-  const matrix = completeJigsaw(rawTiles)
+// Given the raw collection of snapshots, find the right layout to reassemble
+// the jigsaw puzzle (mozaic).
+const solve = snapshots =>
+  snapshots
+    .map(parseSnapshot)
+    .flat()
+    .reduce((acc, tile, _, tiles) => acc || jigsaw(tiles, tile), null)
 
-  return $.product(
-    [
-      matrix[0][0],
-      matrix[0][size - 1],
-      matrix[size - 1][0],
-      matrix[size - 1][size - 1],
-    ].map(cell => cell.id)
-  )
+const run = snapshots => {
+  const mozaic = solve(snapshots)
+  const image = assemble(mozaic)
+
+  return [checksum(mozaic), inspectWaters(image)]
 }
 
-module.exports = { recompose, getFullImageId, findMonsters }
+module.exports = { run }
