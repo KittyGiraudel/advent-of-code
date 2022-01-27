6a3fc1312a27960c2aeb66200c9df2b87a09644e
Kitty Giraudel <kitty.giraudel@gmail.com>
2022-01-27 23:02:15 +0100

    Add day 14

--- src/helpers/memo.js
+++ src/helpers/memo.js
@@ -1,3 +1,11 @@
+const getKey = (...args) =>
+  // If there is only one argument and it’s a safe key, bypass the JSON
+  // serialization as it can be quite costly which would negate the memo
+  // benefits entirely.
+  args.length === 1 && ['string', 'number'].includes(typeof args[0])
+    ? args[0]
+    : JSON.stringify(args)
+
 // Memoize a function’s results using the JSON serialization of its arguments
 // as a key
 // @param {Function} fn - Function to memoize
@@ -6,7 +14,7 @@ const memo = fn => {
   const cache = new Map()
 
   return (...args) => {
-    const key = JSON.stringify(args)
+    const key = getKey(...args)
     if (cache.has(key)) return cache.get(key)
     const result = fn(...args)
     cache.set(key, result)
