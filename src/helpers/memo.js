8837038a64f151b9b4605fc73d98c42d247179fc
Kitty Giraudel <kitty.giraudel@gmail.com>
2022-01-29 14:55:15 +0100

    Add day 16

--- src/helpers/memo.js
+++ src/helpers/memo.js
@@ -1,10 +1,17 @@
 const getKey = (...args) =>
-  // If there is only one argument and it’s a safe key, bypass the JSON
-  // serialization as it can be quite costly which would negate the memo
+  // If there is only one argument and it’s a safe key, or if the second
+  // argument is a number (index when used with Array.prototype.map), bypass the
+  // JSON serialization as it can be quite costly which would negate the memo
   // benefits entirely.
-  args.length === 1 && ['string', 'number'].includes(typeof args[0])
-    ? args[0]
-    : JSON.stringify(args)
+  {
+    const count = args.length
+    const safeKey = ['string', 'number'].includes(typeof args[0])
+    const indexSecond = typeof args[1] === 'number'
+
+    return (count === 1 && safeKey) || (count === 3 && safeKey && indexSecond)
+      ? args[0]
+      : JSON.stringify(args)
+  }
 
 // Memoize a function’s results using the JSON serialization of its arguments
 // as a key
@@ -15,7 +22,10 @@ const memo = fn => {
 
   return (...args) => {
     const key = getKey(...args)
-    if (cache.has(key)) return cache.get(key)
+    if (cache.has(key)) {
+      console.log('Hit for', key)
+      return cache.get(key)
+    }
     const result = fn(...args)
     cache.set(key, result)
     return result
