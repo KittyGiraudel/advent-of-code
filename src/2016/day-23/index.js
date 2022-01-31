b551df9069048e1e150c4847cb87d8a7d6e2748a
Kitty Giraudel <kitty.giraudel@gmail.com>
2022-01-31 10:18:20 +0100

    Add day 23

---
+++ src/2016/day-23/index.js
@@ -0,0 +1,34 @@
+const $ = require('../../helpers')
+
+const run = (instructions, a = 0) => {
+  const registers = { a, b: 0, c: 0, d: 0 }
+  const read = key => registers[key] || +key
+
+  // Speed boost by doing the split only once.
+  instructions = instructions.map(instruction => instruction.split(' '))
+
+  for (let i = 0; i < instructions.length; i++) {
+    const [code, x, y] = instructions[i]
+    console.log(i, code, x, y, registers)
+    if (code === 'cpy' && y in registers) registers[y] = read(x)
+    else if (code === 'inc' && x in registers) registers[x]++
+    else if (code === 'dec' && x in registers) registers[x]--
+    else if (code === 'jnz' && read(x)) i += read(y) - 1
+    else if (code === 'tgl') {
+      const j = i + read(x)
+      const target = instructions[j]
+      if (!target) continue
+      if (target.length === 2) {
+        if (target[0] === 'inc') target[0] = 'dec'
+        else target[0] = 'inc'
+      } else if (target.length === 3) {
+        if (target[0] === 'jnz') target[0] = 'cpy'
+        else target[0] = 'jnz'
+      }
+    }
+  }
+
+  return registers.a
+}
+
+module.exports = { run }
