50a24c509c5ae7305ca4e2276a503e1c0a2bdcf4
Kitty Giraudel <kitty.giraudel@gmail.com>
2022-01-31 10:18:09 +0100

    Add day 22

---
+++ src/2016/day-22/test.js
@@ -0,0 +1,11 @@
+const test = require('ava')
+const { run } = require('./')
+const input = require('../../helpers/readInput')(__dirname)
+
+test.skip('Day 22.1', t => {})
+
+test.skip('Day 22.2', t => {})
+
+test('Day 22 — Solutions', t => {
+  t.is(run(input), 960)
+})
