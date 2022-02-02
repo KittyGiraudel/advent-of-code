3f0a725ecf4f018f753fe2b85592cb13e50544fa
Kitty Giraudel <kitty.giraudel@gmail.com>
2022-02-02 09:39:33 +0100

    Slightly improve day 15

--- src/2021/day-15/index.js
+++ src/2021/day-15/index.js
@@ -1,80 +1,55 @@
 const $ = require('../../helpers')
 
-const createMegaGrid = rows => {
-  const ratio = 5
-  const inc = col => (col === 9 ? 1 : col + 1)
+const getCost = (grid, coords) => {
+  const width = grid[0].length
+  const height = grid.length
+  const [y, x] = coords
+  const yInc = Math.floor(y / height)
+  const xInc = Math.floor(x / width)
 
-  // Generate the entire first row of the mega grid. If the initial grid as 10
-  // rows for instance, this generates a grid of 10 rows * 50 columns.
-  const grid = $.array(rows.length).reduce((acc, _, ri) => {
-    const row = $.array(ratio - 1).reduce(
-      (acc, _, index) => [...acc, acc[index].map(inc)],
-      [rows[ri].split('').map(Number)]
-    )
-
-    return [...acc, row.flat()]
-  }, [])
-
-  // Generate the the missing rows from the mega grid now that all columns are
-  // defined.
-  return $.array(ratio - 1)
-    .reduce(
-      (acc, _, index) => [...acc, acc[index].map(row => row.map(inc))],
-      [grid]
-    )
-    .flat()
+  return ((grid[y % height][x % width] + yInc + xInc - 1) % 9) + 1
 }
 
-const getPath = (grid, start, end) => {
-  const graph = createGraph(grid, start, end)
-  const path = []
-  let current = end
+const getLowestCost = (grid, mapSize = 1) => {
+  const maxWidth = grid[0].length * mapSize - 1
+  const maxHeight = grid.length * mapSize - 1
+  const start = '0,0'
+  const end = $.toPoint([maxHeight, maxWidth])
+  const frontier = new $.PriorityQueue()
+  const costs = { [start]: 0 }
 
-  while (current.point != start.point) {
-    path.push(current.coords)
-    current = graph[current.point]
-  }
+  // Push the starting point into the queue.
+  frontier.push([{ point: start, coords: $.toCoords(start) }, 0])
 
-  return path
-}
-
-const createGraph = (grid, start, end) => {
-  const frontier = new $.PriorityQueue([start, 0])
-  const from = { [start.point]: null }
-  const costs = { [start.point]: 0 }
+  const isWithinBounds = ({ coords: [ri, ci] }) =>
+    $.isClamped(ri, 0, maxHeight) && $.isClamped(ci, 0, maxWidth)
 
   while (frontier.length) {
     const [curr] = frontier.pop()
 
-    if (curr.point === end.point) break
+    if (curr.point === end) break
 
-    $.bordering(curr.coords)
-      .filter(({ coords }) => typeof $.access(grid, coords) === 'number')
-      .forEach(next => {
-        const newCost = costs[curr.point] + $.access(grid, next.coords)
+    const neighbors = $.bordering(curr.coords).filter(isWithinBounds)
 
-        // If the node hasn’t been visited yet, or if the new path is cheaper
-        // than the previously recorded one, visit the node.
-        if (!(next.point in costs) || newCost < costs[next.point]) {
-          const priority = newCost + $.manhattan(end.coords, next.coords)
-          costs[next.point] = newCost
-          from[next.point] = curr
-          frontier.push([next, priority])
-        }
-      })
+    for (let next of neighbors) {
+      const cost = getCost(grid, next.coords)
+      const newCost = costs[curr.point] + cost
+
+      // If the node hasn’t been visited yet, or if the new path is cheaper
+      // than the previously recorded one, visit the node.
+      if (!(next.point in costs) || newCost < costs[next.point]) {
+        // It feels marginally faster without the Manhattan heuristic.
+        // const distance = $.manhattan(end.coords, next.coords)
+        frontier.push([next, newCost /* + distance */])
+        costs[next.point] = newCost
+      }
+    }
   }
 
-  return from
+  return costs[end]
 }
 
-const getLowestRisk = grid => {
-  const height = grid.length - 1
-  const width = grid[0].length - 1
-  const start = { point: '0,0', coords: [0, 0] }
-  const end = { point: height + ',' + width, coords: [height, width] }
-  const path = getPath(grid, start, end)
+const getLowestRisk = (input, mapSize = 1) =>
+  getLowestCost($.grid.create(input, Number), mapSize)
 
-  return $.sum(path.map(coords => $.access(grid, coords)))
-}
-
-module.exports = { createMegaGrid, getLowestRisk }
+module.exports = { getLowestRisk }
