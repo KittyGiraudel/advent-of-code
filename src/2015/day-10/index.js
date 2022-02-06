0d6f23fa9159999f21ba18ad7383648b0086caee
Kitty Giraudel <kitty.giraudel@gmail.com>
2022-02-06 15:48:04 +0100

    Add day 10

---
+++ src/2015/day-10/index.js
@@ -0,0 +1,29 @@
+const $ = require('../../helpers')
+
+const lookAndSay = input => {
+  let output = ''
+  let count = 1
+
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] === input[i + 1]) count++
+    else {
+      output += count + input[i]
+      count = 1
+    }
+  }
+
+  return output
+}
+
+const run = (input, n = 1) => {
+  while (n--) {
+    // The loop version is unsurprisingly faster than the regular expression
+    // version.
+    // input = input.replace(/(\d)\1*/g, (match, char) => match.length + char)
+    input = lookAndSay(input)
+  }
+
+  return input.length
+}
+
+module.exports = { run }
---
+++ src/2015/day-10/test.js
@@ -0,0 +1,17 @@
+const test = require('ava')
+const { run } = require('./')
+
+test('Day 10.1', t => {
+  t.is(run('1'), 2)
+  t.is(run('11'), 2)
+  t.is(run('21'), 4)
+  t.is(run('1211'), 6)
+  t.is(run('111221'), 6)
+})
+
+test.skip('Day 10.2', t => {})
+
+test('Day 10 — Solutions', t => {
+  t.is(run('1113122113', 40), 360154)
+  t.is(run('1113122113', 50), 5103798)
+})
