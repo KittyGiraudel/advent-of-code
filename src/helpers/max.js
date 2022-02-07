0c4af5c7bd20205a626c3c2239d33d1066728ef5
Kitty Giraudel <kitty.giraudel@gmail.com>
2022-02-07 17:40:30 +0100

    Add day 15

---
+++ src/helpers/max.js
@@ -0,0 +1,7 @@
+// `Math.max(…)` causes a ‘Maximum call stack size exceeded’ error when passing
+// too many items, so we have to resort to a good ol’ loop.
+// @param {Number[]} array - Array to find the maximum value of
+// @return {Number}
+const max = array => array.reduce((a, b) => Math.max(a, b), -Infinity)
+
+module.exports = max
