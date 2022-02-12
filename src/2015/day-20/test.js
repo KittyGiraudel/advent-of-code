1fa9f76058cc44d6464235ffe7250c12417f3247
Kitty Giraudel <kitty.giraudel@gmail.com>
2022-02-12 23:06:23 +0100

    Add day 20

---
+++ src/2015/day-20/test.js
@@ -0,0 +1,10 @@
+const test = require('ava')
+const { run } = require('./')
+
+test('Day 20.1', t => {
+  t.deepEqual(run(150), [8, 8])
+})
+
+test('Day 20 — Solutions', t => {
+  t.deepEqual(run(29_000_000), [665280, 705600])
+})
