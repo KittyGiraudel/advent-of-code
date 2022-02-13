8a8cbc90e91cca464583963b167fd867579bb371
Kitty Giraudel <kitty.giraudel@gmail.com>
2022-02-13 15:39:59 +0100

    Add day 24

---
+++ src/2015/day-24/test.js
@@ -0,0 +1,17 @@
+const test = require('ava')
+const { run } = require('./')
+const input = require('../../helpers/readInput')(__dirname).map(Number)
+
+const sample = [1, 2, 3, 4, 5, 7, 8, 9, 10, 11]
+
+test('Day 24.1', t => {
+  t.is(run(sample, 3), 99)
+  t.is(run(sample, 4), 44)
+})
+
+test.skip('Day 24.2', t => {})
+
+test('Day 24 — Solutions', t => {
+  t.is(run(input, 3), 10439961859)
+  t.is(run(input, 4), 72050269)
+})
