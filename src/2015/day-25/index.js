4f9911697f83e39640b1f99f6f3a41e42e0893a7
Kitty Giraudel <kitty.giraudel@gmail.com>
2022-02-13 16:00:46 +0100

    Add day 25

---
+++ src/2015/day-25/index.js
@@ -0,0 +1,19 @@
+const $ = require('../../helpers')
+
+const run = ([row, col]) => {
+  let curr = 20151125
+  let diagonal = 1
+
+  // The grid is filled diagonally, so iterate over the diagonals endlessly
+  // until the right cell has been found.
+  while (diagonal++) {
+    // Iterate on the current diagonal to compute the next value.
+    for (let i = 0; i < diagonal; i++) {
+      curr = (curr * 252533) % 33554393
+      // If we’re on the target, return the value.
+      if (diagonal - i === row && i + 1 === col) return curr
+    }
+  }
+}
+
+module.exports = { run }
