0ad1f599979bad7f6e579e425b816122d5188e6b
Kitty Giraudel <kitty.giraudel@gmail.com>
2022-11-30 10:24:08 +0100

    Prepare 2022

---
+++ src/2022/day-01/test.js
@@ -0,0 +1,9 @@
+const test = require('ava')
+const {} = require('./')
+const input = require('../../helpers/readInput')(__dirname)
+
+test('Day 1.1', t => {})
+
+test('Day 1.2', t => {})
+
+test('Day 1 — Solutions', t => {})
