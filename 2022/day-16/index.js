1f0bdf9d48d907596a5299251b80a9976cf3dbab
Kitty Giraudel <kitty@cofenster.com>
2022-12-17 17:38:10 +0100

    Add day 16 2022

---
+++ 2022/day-16/index.js
@@ -0,0 +1,82 @@
+const $ = require('../../helpers')
+
+const releasePressure = input => {
+  const map = input.reduce((acc, line) => {
+    const [, name, flow, tunnels] = line.match(
+      /Valve (\w+) has flow rate=(\d+); tunnels? leads? to valves? ([\w,\s]+)/
+    )
+    const valve = { name, flow: +flow, tunnels: tunnels?.split(/,\s/g) }
+
+    return { ...acc, [name]: valve }
+  }, {})
+
+  const getDistances = startName => {
+    const walk = (acc, { name, steps }) => {
+      if (name in acc && acc[name] <= steps) return acc
+      acc[name] = steps
+
+      return map[name].tunnels.reduce(
+        (acc, name) => walk(acc, { name, steps: steps + 1 }),
+        acc
+      )
+    }
+
+    return walk({}, { name: startName, steps: 0 })
+  }
+
+  const distanceMap = Object.keys(map).reduce(
+    (acc, name) => ({ ...acc, [name]: getDistances(name) }),
+    {}
+  )
+
+  const frontier = [
+    {
+      name: 'AA',
+      remaining: Object.values(map)
+        .filter(node => node.flow > 0)
+        .map(node => node.name),
+      time: 30,
+      steps: [],
+      pressure: 0,
+    },
+  ]
+
+  let max = 0
+
+  while (frontier.length) {
+    const curr = frontier.pop()
+
+    if (curr.time <= 0) curr.finished = true
+    if (curr.finished) continue
+
+    let moved = false
+    let distances = distanceMap[curr.name]
+
+    curr.remaining
+      .filter(
+        name =>
+          name !== curr.name && // Not self
+          curr.time - distances[name] > 1 // Within time
+      )
+      .forEach(name => {
+        moved = true
+        let time = curr.time - distances[name] - 1
+
+        frontier.unshift({
+          name,
+          time,
+          finished: false,
+          remaining: curr.remaining.filter(node => node !== name),
+          steps: [...curr.steps, name],
+          pressure: curr.pressure + time * map[name].flow,
+        })
+      })
+
+    if (!moved) curr.finished = true
+    if (curr.finished && curr.pressure > max) max = curr.pressure
+  }
+
+  return max
+}
+
+module.exports = { releasePressure }
---
+++ 2022/day-16/test.js
@@ -0,0 +1,24 @@
+const test = require('ava')
+const { releasePressure } = require('./')
+const input = require('../../helpers/readInput')(__dirname)
+
+const sample = `Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+Valve BB has flow rate=13; tunnels lead to valves CC, AA
+Valve CC has flow rate=2; tunnels lead to valves DD, BB
+Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+Valve EE has flow rate=3; tunnels lead to valves FF, DD
+Valve FF has flow rate=0; tunnels lead to valves EE, GG
+Valve GG has flow rate=0; tunnels lead to valves FF, HH
+Valve HH has flow rate=22; tunnel leads to valve GG
+Valve II has flow rate=0; tunnels lead to valves AA, JJ
+Valve JJ has flow rate=21; tunnel leads to valve II`.split('\n')
+
+test('Day 16.1', t => {
+  t.is(releasePressure(sample), 1651)
+})
+
+test.skip('Day 16.2', t => {})
+
+test('Day 16 — Solutions', t => {
+  t.is(releasePressure(input), 1944)
+})
