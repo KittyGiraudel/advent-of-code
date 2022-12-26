4b66ffa133f3c563f6c52d67a0303b321b232ef3
Kitty Giraudel <kitty.giraudel@gmail.com>
2022-12-26 17:08:10 +0100

    Add day 22 2016

--- 2016/day-22/index.js
+++ 2016/day-22/index.js
@@ -1,7 +1,7 @@
 const $ = require('../../helpers')
 
-const run = dump => {
-  const nodes = dump.slice(2).map(line => {
+const getDisks = dump =>
+  dump.slice(2).map(line => {
     const [, x, y, size, used, available] = line
       .match(/x(\d+)-y(\d+)\s+(\d+)T\s+(\d+)T\s+(\d+)T\s+(\d+)/)
       .map(Number)
@@ -9,11 +9,73 @@ const run = dump => {
     return { coords: [y, x], size, used, available }
   })
 
-  return $.combinations(nodes, 2).filter(
+const run = dump => {
+  const disks = getDisks(dump)
+
+  return $.combinations(disks, 2).filter(
     ([a, b]) =>
       (a.used > 0 && a.used <= b.available) ||
       (b.used > 0 && b.used <= a.available)
   ).length
 }
 
-module.exports = { run }
+// I was a little stumped with part 2, so I originally solved it manually with
+// this online visualization: https://codepen.io/anon/pen/BQEZzK/?editors=0010
+// Once I understood it worked, I decided to give a proper solution a go. There
+// are *a lot* of asumptions for this solution to work though, not to mention
+// hard-coded details. Namely:
+// 1. It relies on the data and goal being on the same row.
+// 2. It assumes there is only a single empty disk in the grid.
+// 3. It assumes there are no oversized disks on the top row.
+// The idea is to move the empty disk around, to the left of the data disk. Then
+// swap both disks. Repeat until the data is in the goal spot (top-left corner).
+const getData = dump => {
+  const disks = getDisks(dump)
+  const maxCi = Math.max(...disks.map(disk => disk.coords[1]))
+  const emptyNode = disks.find(disk => disk.used === 0)
+  const oversizedDisks = disks.filter(disk => disk.used > emptyNode.size)
+  const availableDisks = disks
+    .filter(disk => disk.used <= emptyNode.size)
+    .map(disk => disk.coords)
+    .map($.toPoint)
+
+  let data = [0, maxCi]
+  let curr = emptyNode.coords
+  let total = 0
+
+  const getNeighbors = curr =>
+    $.bordering(curr, 'COORDS').filter(coords => {
+      const point = $.toPoint(coords)
+      return availableDisks.includes(point) && point !== $.toPoint(data)
+    })
+
+  // While the data is not in the top-left corner (coords 0,0), repeatedly
+  // position the empty disk on its left, and swap both disks.
+  while (data[0] || data[1]) {
+    // Find the path from the empty disk to the left of the data disk.
+    const end = [data[0], data[1] - 1]
+    const graph = $.pathfinding.search({ start: curr, end, getNeighbors })
+
+    // Increment the total amount of moves by the length of this path.
+    total += $.pathfinding.path(graph.from, curr, end).length
+
+    // Swap both disks for an additional move.
+    curr = data
+    data = end
+    total += 1
+
+    // If we can guarantee there are no oversized disks on the second row, we
+    // can shorten that loop entirely by computing the remaining steps once we
+    // have the goal (0,0), the empty disk (`_`) and the data (`D`) on the same
+    // row. Swapping the empty disk with the data takes 5 moves:
+    //          1           2           3           4           5
+    // y=0    . D _       . D .       . D .       . D .       _ D .
+    // y=1    . . .       . . _       . _ .       _ . .       . . .
+    if (!oversizedDisks.some(disk => disk.coords[0] === 1))
+      return total + (maxCi - 1) * 5
+  }
+
+  return total
+}
+
+module.exports = { run, getData }
--- 2016/day-22/test.js
+++ 2016/day-22/test.js
@@ -1,9 +1,10 @@
 const test = require('ava')
-const { run } = require('./')
+const { run, getData } = require('./')
 const input = require('../../helpers/readInput')(__dirname)
 
 test.skip('Day 22 — Sample', t => {})
 
 test('Day 22 — Solutions', t => {
   t.is(run(input), 960)
+  t.is(getData(input), 225)
 })
