dbb3319f96b06e89870a555de922866db3609642
Kitty Giraudel <kitty.giraudel@gmail.com>
2022-12-28 11:52:44 +0100

    Fix incorrect test label

--- 2016/day-13/test.js
+++ 2016/day-13/test.js
@@ -1,10 +1,10 @@
 const test = require('ava')
 const { run } = require('./')
 
-test('Day 12 — Sample', t => {
+test('Day 13 — Sample', t => {
   t.is(run([4, 7], 10)[0], 11)
 })
 
-test('Day 12 — Solutions', t => {
+test('Day 13 — Solutions', t => {
   t.deepEqual(run([39, 31], 1364), [86, 127])
 })
