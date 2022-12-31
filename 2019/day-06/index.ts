8c2f4d722b4b51a43b2f29eee12d1eab8cfa372a
Kitty Giraudel <kitty.giraudel@gmail.com>
2022-12-31 18:02:31 +0100

    Rewrite 2019/day-06 using the BFS helper

--- 2019/day-06/index.ts
+++ 2019/day-06/index.ts
@@ -22,24 +22,50 @@ export const getPaths = (
   end: string = 'COM',
   path: string[] = []
 ): string[][] => {
-  if (curr === end) return [[...path, curr]]
+  if (curr === end) return [path]
   if (path.includes(curr)) return []
   return Array.from(graph.get(curr)).reduce(
-    (acc, sibling) =>
-      acc.concat(getPaths(graph, sibling, end, [...path, curr])),
+    (acc, next) => acc.concat(getPaths(graph, next, end, [...path, curr])),
     []
   )
 }
 
-export const countOrbits = (graph: Graph): number =>
-  Array.from(graph.keys()).reduce(
+export const countOrbits = (graph: Graph, to: string = 'COM'): number => {
+  return Array.from(graph.keys()).reduce((orbits, key) => {
+    const { from } = $.pathfinding.search({
+      start: key,
+      isDone: curr => curr === to,
+      getNeighbors: curr => Array.from(graph.get(curr)),
+    })
+
+    return orbits + $.pathfinding.path(from, key, to).length
+  }, 0)
+
+  // This is my original version, which recursively walks the graph. It is
+  // incredibly slower than the newer version using the BFS utility though
+  // (11s vs. <1s).
+  return Array.from(graph.keys()).reduce(
     (total, key) =>
-      total + $.sum(getPaths(graph, key, 'COM').map(path => path.length - 1)),
+      total + $.sum(getPaths(graph, key, 'COM').map(path => path.length)),
     0
   )
+}
 
 export const countTransfers = (
   graph: Graph,
   start: string = 'YOU',
   end: string = 'SAN'
-): number => getPaths(graph, start, end)[0].length - 3
+): number => {
+  const { from } = $.pathfinding.search({
+    start,
+    isDone: curr => curr === end,
+    getNeighbors: curr => Array.from(graph.get(curr)),
+  })
+  const path = $.pathfinding.path(from, start, end)
+
+  // This is the original version, which is much slower.
+  // const [path] = getPaths(graph, start, end)
+
+  // Drop the 2 extremities as they shouldn’t count in the amount of transfers.
+  return path.length - 2
+}
