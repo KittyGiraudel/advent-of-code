9b0f761da667bf9d73c8f60ac2ae45404e3164be
Kitty Giraudel <kitty.giraudel@gmail.com>
2023-11-28 20:21:19 +0100

    Update Node to v20

---
+++ 2019/day-18/index.ts
@@ -0,0 +1,104 @@
+import $ from '../../helpers'
+import { Coords, CoordsAndPoint, Point } from '../../types'
+
+type Node = {
+  coords: Coords
+  keys: Set<string>
+  steps: number
+}
+
+const isKey = (value: string): boolean => /[a-z]/.test(value)
+const isDoor = (value: string): boolean => /[A-Z]/.test(value)
+
+export const maze = (input: string[]) => {
+  const keys: Record<Point, string> = {}
+  const startCoords: Coords = [0, 0]
+  const grid = $.grid.create<string>(input, (value, ri, ci) => {
+    if (value === '@') {
+      startCoords[0] = ri
+      startCoords[1] = ci
+      return '.'
+    }
+    if (isKey(value)) {
+      keys[$.toPoint([ri, ci])] = value
+      return '.'
+    }
+    return value
+  })
+  const keyCount = Object.keys(keys).length
+  const start: Node = { coords: startCoords, keys: new Set(), steps: 0 }
+  // const frontier: Node[] = [start]
+
+  console.log(keys, startCoords)
+
+  const { from, end } = $.pathfinding.dijkstra({
+    start,
+    isGoal: curr => curr.keys.size === keyCount,
+    toKey: curr => $.toPoint(curr.coords),
+    getCost: curr => curr.steps + 1,
+    getNextNodes: curr =>
+      $.bordering(curr.coords, 'BOTH')
+        .filter((next: CoordsAndPoint) => {
+          const value = $.access(grid, next.coords)
+          const isOpenDoor = isDoor(value) && curr.keys.has(value.toLowerCase())
+
+          return value === '.' || isOpenDoor
+        })
+        .map((next: CoordsAndPoint) => {
+          const nextKeys =
+            next.point in keys
+              ? new Set(curr.keys).add(keys[next.point])
+              : curr.keys
+
+          return { coords: next.coords, keys: nextKeys, steps: curr.steps + 1 }
+        }),
+  })
+
+  console.log(from, end)
+  /*
+  while (frontier.length) {
+    const curr = frontier.pop()
+
+    if (curr.keys.size === keyCount) {
+      end = curr
+      break
+    }
+
+    console.log(curr)
+
+    const neighbors = $.bordering(curr.coords, 'BOTH')
+    const available = neighbors.filter((next: CoordsAndPoint) => {
+      const value = $.access(grid, next.coords)
+
+      return (
+        value === '.' || (isDoor(value) && curr.keys.has(value.toLowerCase()))
+      )
+    })
+
+    available.forEach((next: CoordsAndPoint) => {
+      const nextKeys =
+        next.point in keys
+          ? new Set(curr.keys).add(keys[next.point])
+          : curr.keys
+
+      frontier.unshift({
+        coords: next.coords,
+        keys: nextKeys,
+        steps: curr.steps + 1,
+      })
+    })
+  }
+
+  return end.steps
+  */
+}
+
+maze(
+  $.sample(`
+########################
+#f.D.E.e.C.b.A.@.a.B.c.#
+######################.#
+#d.....................#
+########################
+`)
+)
--- 2021/day-19/index.ts
+++ 2021/day-19/index.ts
@@ -1,72 +1,20 @@
 import $ from '../../helpers'
 import { Coords, Point } from '../../types'
 
-const findVector = (a: Coords, b: Coords): Coords => [
-  b[0] - a[0],
-  b[1] - a[1],
-  b[2] - a[2],
-]
-
 class Scanner {
-  id: number
-  beacons: Coords[]
-  vectors: Map<Point, Set<Point>>
+  points: Point[]
+  coords: Coords[]
 
-  constructor(scanner: string) {
-    const [header, ...lines] = scanner.split('\n')
-
-    this.id = +header.match(/(\d+)/)[1]
-    this.beacons = lines.map($.toCoords)
+  constructor(raw: string) {
+    const lines = raw.split('\n')
+    this.points = lines.slice(1) as Point[]
+    this.coords = this.points.map($.toCoords)
   }
 
-  findVectorsBetweenBeacons() {
-    const pairs = $.combinations(this.beacons, 2)
-    const vectors: Map<Point, Set<Point>> = new Map()
-
-    pairs.forEach(([a, b]) => {
-      const vectorsA = vectors.get($.toPoint(a)) || new Set()
-      const vectorsB = vectors.get($.toPoint(b)) || new Set()
-
-      vectorsA.add($.toPoint(findVector(a, b)))
-      vectorsB.add($.toPoint(findVector(b, a)))
-
-      vectors.set($.toPoint(a), vectorsA)
-      vectors.set($.toPoint(b), vectorsB)
-    })
-
-    this.vectors = vectors
-  }
+  scan() {}
 }
 
-export const findOverlaps = (scannerA: Scanner, scannerB: Scanner) => {
-  const overlaps = new Map()
-  const beaconPairs = Array.from({ length: scannerA.beacons.length }).map(
-    (_, i) => [scannerA.beacons[i], scannerB.beacons[i]]
-  )
-
-  beaconPairs.forEach(([a, b]) => {
-    const vectorsA = scannerA.vectors.get($.toPoint(a))
-    const vectorsB = scannerB.vectors.get($.toPoint(b))
-
-    const intersection = $.intersection(
-      Array.from(vectorsA),
-      Array.from(vectorsB)
-    )
-
-    if (intersection.length >= 12) {
-      const overlapsA = overlaps.get($.toPoint(a))
-      overlapsA.add($.toPoint(b))
-      overlaps.set($.toPoint(a), overlapsA)
-    }
-  })
-
-  return overlaps
-}
-
-export const parse = (scanners: string[]) => {
-  return scanners.map(scanner => {
-    const s = new Scanner(scanner)
-    s.findVectorsBetweenBeacons()
-    return s
-  })
+const parse = (input: string[]) => {
+  const scanners = input.forEach(raw => new Scanner(raw))
+  const pairs = $.combinations(scanners, 2)
 }
