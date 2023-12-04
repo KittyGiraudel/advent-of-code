7910cfda87405185696b84a39f8a09802fda1a1f
Kitty Giraudel <kitty.giraudel@gmail.com>
2023-12-04 09:20:53 +0100

    Add Day 04 2023

---
+++ 2023/day-04/index.ts
@@ -0,0 +1,29 @@
+import $ from '../../helpers'
+
+export const parseCard = (line: string) => {
+  const sets = line.split(':')[1].split('|').map($.numbers)
+  const score = $.intersection(...sets).length
+
+  return { score, count: 1 }
+}
+
+export const getDeckScore = (input: string[], advanced?: boolean) => {
+  const cards = input.map(parseCard)
+
+  if (advanced) {
+    // Iterate through the deck sequentially. For each card, iterate as many
+    // times as it had winning numbers, and increase the amount of copies of the
+    // subsequent cards in the deck. For instance, if cards (plural intended) #4
+    // have 3 winning numbers (a score of 3), increase the count of card #5, #6,
+    // and #7 by the amount of cards #4 we own.
+    for (let i = 0; i < cards.length; i++)
+      for (let j = 1; j <= cards[i].score; j++)
+        cards[i + j].count += cards[i].count
+  }
+
+  const scores = advanced
+    ? cards.map(card => card.count)
+    : cards.map(card => (card.score ? 2 ** (card.score - 1) : 0))
+
+  return $.sum(scores)
+}
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 <!-- AOC TILES BEGIN -->
 <h1 align="center">
-  2023 - 6 ⭐
+  2023 - 8 ⭐
 </h1>
 <a href="2023/day-01/index.ts">
   <img src="Media/2023/01.png" alt="Day 1 2023: Complete" width="161px">
@@ -12,6 +12,9 @@
 </a>
 <a href="2023/day-03/index.ts">
   <img src="Media/2023/03.png" alt="Day 3 2023: Complete" width="161px">
+</a>
+<a href="2023/day-04/index.ts">
+  <img src="Media/2023/04.png" alt="Day 4 2023: Complete" width="161px">
 </a>
 <h1 align="center">
   2022 - 46 ⭐
