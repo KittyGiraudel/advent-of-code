43b05978ae611af20c25e584593d2f0967cc2637
Kitty Giraudel <kitty.giraudel@gmail.com>
2023-12-09 16:15:07 +0100

    Fix TS issues

--- helpers/frequency.ts
+++ helpers/frequency.ts
@@ -1,17 +1,18 @@
+type Frequencies = Record<string, number>
+
 /**
  * Count the occurrences of every item in the given array or given stirng and
  * return an object mapping items/characters to their count.
  */
-function frequency(input: string): Record<string, number>
-function frequency<T>(input: T[]): Record<string, number>
-function frequency<T>(input: string | T[]): Record<string, number> {
+function frequency(input: string): Frequencies
+function frequency<T>(input: T[]): Frequencies
+function frequency<T>(input: string | T[]): Frequencies {
   const array = Array.isArray(input) ? input : Array.from(input)
-  const frequencies = array.reduce<Record<string, number>>((acc, curr) => {
-    const key = String(curr)
-    if (key in acc) acc[key]++
-    else acc[key] = 1
-    return acc
-  }, {})
+  const keys = array.map(String)
+  const frequencies = keys.reduce<Frequencies>(
+    (acc, curr) => ({ ...acc, [curr]: (acc[curr] ?? 0) + 1 }),
+    {}
+  )
 
   return frequencies
 }
