f83b1137a29b3bcaf0d0bef652b0a9c9ea46cc37
Kitty Giraudel <kitty@cofenster.com>
2023-12-15 10:13:15 +0100

    Add Day 15 2023

---
+++ 2023/day-15/index.ts
@@ -0,0 +1,51 @@
+import $ from '../../helpers'
+
+const hash = (input: string) => {
+  let value = 0
+
+  for (let i = 0; i < input.length; i++) {
+    value += input.charCodeAt(i)
+    value *= 17
+    value %= 256
+  }
+
+  return value
+}
+
+type Lens = { type: '=' | '-'; label: string; id: number; value: number }
+
+const parse = (input: string) => {
+  const [label, type, rest] = input.split(/([=-])/)
+  const id = hash(label)
+  const value = rest ? +rest : Infinity
+  return { type, label, id, value } as Lens
+}
+
+export const run = (input: string[], advanced: boolean = false) => {
+  if (!advanced) {
+    return $.sum(input.map(hash))
+  }
+
+  const boxes: Map<number, Lens[]> = new Map()
+
+  input.map(parse).forEach(lens => {
+    if (!boxes.has(lens.id)) boxes.set(lens.id, [])
+
+    const box = boxes.get(lens.id)!
+    const index = box.findIndex(({ label }) => lens.label === label)
+    const newIndex = index > -1 ? index : box.length
+
+    // My first version was a bit more verbose so I golfed it a bit for funsies.
+    // The gist is that we remove the lens if it’s already in there, and if it’s
+    // an = lens, we insert it at the right position (where the removed lens was
+    // or at the end otherwise).
+    if (index > -1) box.splice(index, 1)
+    if (lens.type === '=') box.splice(newIndex, 0, lens)
+  })
+
+  return $.sum(
+    Array.from(boxes.values()).flatMap(lenses =>
+      lenses.map(({ id, value }, index) => (1 + id) * (1 + index) * value)
+    )
+  )
+}
---
+++ 2023/day-15/test.ts
@@ -0,0 +1,21 @@
+import test from 'ava'
+import $ from '../../helpers'
+import { run } from './'
+
+test('Day 15 — Sample', t => {
+  const sample = $.sample(
+    `rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7`,
+    { delimiter: ',' }
+  )
+  const sample2 = $.sample('HASH')
+
+  t.is(run(sample2), 52)
+  t.is(run(sample), 1320)
+  t.is(run(sample, true), 145)
+})
+
+test('Day 15 — Solutions', t => {
+  const input = $.readInput(import.meta, { delimiter: ',' })
+  t.is(run(input), 521_341)
+  t.is(run(input, true), 252_782)
+})
