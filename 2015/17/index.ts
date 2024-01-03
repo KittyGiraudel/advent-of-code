b61be5ce375eb5a997f860f135140d99df4dc974
Kitty Giraudel <kitty@cofenster.com>
2024-01-03 16:57:39 +0100

    Add additional version for 2015/17

--- 2015/17/index.ts
+++ 2015/17/index.ts
@@ -25,3 +25,27 @@ export const run = (input: number[], target: number): [number, number] => {
     arrangements.filter(a => a.length === min).length,
   ]
 }
+
+// Here is another version written much much later which I find a bit more
+// readable and understandable (and which is probably faster on larger sets).
+// The idea is to generate all arrangements of bottles of size N (starting from
+// 1 until reaching the length of the input), and then checking if that
+// arrangement meets the target by summing its components.
+/*
+const run = (input: number[], part2: boolean = false) => {
+  let possibilities = 0
+
+  for (let i = 1; i <= input.length; i++) {
+    let subtotal = 0
+
+    for (const combination of $.combinations(input, i)) {
+      if ($.sum(combination) === 150) subtotal += 1
+    }
+
+    if (part2 && subtotal) return subtotal
+    possibilities += subtotal
+  }
+
+  return possibilities
+}
+*/
