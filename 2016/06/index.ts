afedd20445a020e8d7c7380e293b11d3976dc3ed
Kitty Giraudel <kitty@cofenster.com>
2024-01-04 14:08:14 +0100

    Improve a variable name in Day 06 2016

--- 2016/06/index.ts
+++ 2016/06/index.ts
@@ -1,7 +1,7 @@
 import $ from '../../helpers'
 
 export const run = (lines: string[], part2: boolean = false) => {
-  const columns = $.range(lines[0].length).map(i => $.column(lines, i))
+  const columns = $.range(lines[0].length).map(ci => $.column(lines, ci))
   const counters = columns.map(column =>
     Object.entries($.frequency(column)).sort((a, b) => a[1] - b[1])
   )
