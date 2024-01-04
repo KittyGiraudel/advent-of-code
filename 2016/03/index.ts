b8fe7537f9c1f481b363377960149bba845f1639
Kitty Giraudel <kitty@cofenster.com>
2024-01-04 14:08:00 +0100

    Simplify Day 03 2016

--- 2016/03/index.ts
+++ 2016/03/index.ts
@@ -2,8 +2,10 @@ import $ from '../../helpers'
 
 const isValid = ([a, b, c]: number[]) => a + b > c && a + c > b && b + c > a
 
-export const run = (input: string[], part2: boolean = false) =>
-  (part2
-    ? $.chunk(input.map($.numbers), 3).flatMap($.zip)
-    : input.map($.numbers)
-  ).filter(isValid).length
+export const run = (input: string[], part2: boolean = false) => {
+  const rows = input.map($.numbers)
+  const columns = $.range(rows[0].length).map(ci => $.column(rows, ci))
+  const entries = part2 ? $.chunk(columns.flat(), 3) : rows
+
+  return entries.filter(isValid).length
+}
