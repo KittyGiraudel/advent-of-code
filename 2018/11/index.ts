0d8e8e536ae8a7d2357654599219b3b22984a909
Kitty Giraudel <kitty@cofenster.com>
2024-01-05 11:37:23 +0100

    Use ri,ci instead of x,y in Day 11 2018

--- 2018/11/index.ts
+++ 2018/11/index.ts
@@ -18,16 +18,18 @@ export const getFuelStrict = (serial: number) => {
   const max = { value: -Infinity, coords: [0, 0] as Coords }
   const grid = getGrid(serial)
 
-  const square = (x: number, y: number, size: number = 3) =>
-    $.sum(grid.rows.slice(y, y + size).flatMap(row => row.slice(x, x + size)))
+  const square = (ci: number, ri: number, size: number = 3) =>
+    $.sum(
+      grid.rows.slice(ri, ri + size).flatMap(row => row.slice(ci, ci + size))
+    )
 
-  for (let y = 0; y < SIZE; y++) {
-    for (let x = 0; x < SIZE; x++) {
-      const value = square(x, y, 3)
+  for (let ri = 0; ri < SIZE; ri++) {
+    for (let ci = 0; ci < SIZE; ci++) {
+      const value = square(ci, ri, 3)
 
       if (value > max.value) {
         max.value = value
-        max.coords = [y + 1, x + 1]
+        max.coords = [ri + 1, ci + 1]
       }
     }
   }
@@ -47,12 +49,12 @@ export const getFuelLoose = (serial: number) => {
   // reports the sum of all points above and to the left of (y, x).
   // The following algorithm is inspired from this Ruby version:
   // https://www.reddit.com/r/adventofcode/comments/a53r6i/comment/ebjsc3u/?utm_source=reddit&utm_medium=web2x&context=3
-  summed.forEach((v, [ri, ci]) => {
+  summed.forEach((value, [ri, ci]) => {
     const get = (...coords: Coords) => summed.get(coords) ?? 0
 
     summed.set(
       [ri, ci],
-      v + get(ri - 1, ci) + get(ri, ci - 1) - get(ri - 1, ci - 1)
+      value + get(ri - 1, ci) + get(ri, ci - 1) - get(ri - 1, ci - 1)
     )
   })
 
@@ -60,19 +62,22 @@ export const getFuelLoose = (serial: number) => {
     const ranges = $.range(SIZE - i, 1)
 
     // Iterate over the Y axis.
-    ranges.forEach(yMin => {
-      const yMins = summed.row(yMin - 1)
-      const yMaxes = summed.row(yMin - 1 + i)
+    ranges.forEach(riMin => {
+      const riMins = summed.row(riMin - 1)
+      const riMaxes = summed.row(riMin - 1 + i)
 
       // Iterate over the X axis.
-      ranges.forEach(xMin => {
-        const xMax = xMin + i - 1
+      ranges.forEach(ciMin => {
+        const ciMax = ciMin + i - 1
         const power =
-          yMaxes[xMax] - yMins[xMax] - yMaxes[xMin - 1] + yMins[xMin - 1]
+          riMaxes[ciMax] -
+          riMins[ciMax] -
+          riMaxes[ciMin - 1] +
+          riMins[ciMin - 1]
 
         if (power > max.value) {
           max.value = power
-          max.coords = [yMin + 1, xMin + 1, i]
+          max.coords = [riMin + 1, ciMin + 1, i]
         }
       })
     })
