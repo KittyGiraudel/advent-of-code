e9f14cc7e2e2ba9d6b59186ed9e84954395429fc
Kitty Giraudel <kitty@cofenster.com>
2024-01-05 15:37:14 +0100

    Make the ‘replace’ helper return the same type as the input

--- helpers/replace.ts
+++ helpers/replace.ts
@@ -4,22 +4,29 @@ import { Coords, QuadriCoords, TriCoords } from '../types'
  * Return a new array with the value at given index updated with the given
  * value.
  */
-function replace(string: string, index: number, value: string): string
-function replace<T>(array: T[], index: number, value: T): T[]
-function replace(coords: Coords, index: number, value: number): Coords
-function replace(coords: TriCoords, index: number, value: number): TriCoords
+function replace(input: string, index: number, value: string): typeof input
+function replace(input: Coords, index: number, value: number): typeof input
+function replace(input: TriCoords, index: number, value: number): typeof input
 function replace(
-  coords: QuadriCoords,
+  input: QuadriCoords,
   index: number,
   value: number
-): QuadriCoords
+): typeof input
+function replace<T>(input: T[], index: number, value: T): typeof input
 function replace<T>(
   input: string | Coords | TriCoords | QuadriCoords | T[],
   index: number,
   value: string | number | T
-) {
-  const next = [...input.slice(0, index), value, ...input.slice(index + 1)]
-  return typeof input === 'string' ? next.join('') : next
+): typeof input {
+  if (typeof input === 'string') {
+    return [...input.slice(0, index), value, ...input.slice(index + 1)].join('')
+  }
+
+  return [
+    ...input.slice(0, index),
+    value,
+    ...input.slice(index + 1),
+  ] as typeof input
 }
 
 export default replace
