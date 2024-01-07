9a5747af78437b095aaad570ba7bf403ed8b6828
Kitty Giraudel <kitty.giraudel@gmail.com>
2024-01-07 17:05:04 +0100

    Simplify Day 14 2020

--- 2020/14/index.ts
+++ 2020/14/index.ts
@@ -1,92 +1,47 @@
 import $ from '../../helpers'
 
-type Instruction = string | number[]
-type Memory = Record<string, number>
-type Program = [Memory, string | null]
+// This is a complete rewrite done years later, which is significantly faster
+// and easier to understand, not to mention doesn’t require some `@ts-ignore`
+// comments. Glad I managed to clean it up because I remember it giving me
+// troubles back then.
+export const run = (input: string[], part2: boolean = false) => {
+  const memory = new Map<number, number>()
+  const toBinary = (value: number) => String($.toBin(value)).padStart(36, '0')
+  let mask: string = ''
 
-// Parse the given input into a comprehensible set of instructions.
-// @param input - Input data
-export const parseProgram = (input: string[]) =>
-  input.map(line =>
-    line.startsWith('mask')
-      ? line.replace('mask = ', '')
-      : $.match(line, /mem\[(\d+)\] = (\d+)/)
-          .slice(1)
-          .map(Number)
-  ) as Instruction[]
+  input.forEach(line => {
+    if (line.startsWith('mask = ')) mask = line.split(' = ')[1]
+    else {
+      const [index, value] = $.numbers(line)
+      const binary = part2 ? toBinary(index) : toBinary(value)
 
-// Apply given mask to given value.
-// @param value - Value as a base-10 integers
-// @param mask - Bitmask
-const applyMask = (value: number, mask: string) =>
-  $.toDec(
-    $.stringMap($.toBin(value), (char, i) => (mask[i] === 'X' ? char : mask[i]))
-  )
+      if (!part2) {
+        const masked = $.toDec(
+          $.stringMap(mask, (char, i) => (char === 'X' ? binary[i] : char))
+        )
+        memory.set(index, masked)
+      } else {
+        Array.from(mask)
+          .reduce(
+            (addresses, char, index) => {
+              if (char === '0') {
+                return addresses.map(address => address + binary[index])
+              } else if (char === '1') {
+                return addresses.map(address => address + '1')
+              } else {
+                return addresses.flatMap(address => [
+                  address + '1',
+                  address + '0',
+                ])
+              }
+            },
+            ['']
+          )
+          .map($.toDec)
+          .forEach(index => memory.set(index, value))
+      }
+    }
+  })
 
-// Loose processor: the mask is used to apply a value at given memory location.
-// @param memory - Memory object
-// @param mask - Bitmask
-// @param instruction - Mask (string value) or memory (key,
-//                                        value pair of numbers) instruction
-// @return Memory and mask for further processing
-export const processLoose = (
-  [memory, mask]: Program,
-  instruction: Instruction
-) => {
-  if (typeof instruction === 'string') return [memory, instruction] as Program
-  memory[String(instruction[0])] = applyMask(instruction[1], mask!)
-  return [memory, mask] as Program
-}
-
-// Resolve wildcard characters (`X`) in given address to expand it into multiple
-// possible addresses.
-// @param value - Masked value
-const resolveAddresses = (value: string[]): string[][] =>
-  value.includes('X')
-    ? [
-        ...resolveAddresses($.replace(value, value.indexOf('X'), '0')),
-        ...resolveAddresses($.replace(value, value.indexOf('X'), '1')),
-      ]
-    : [value]
-
-// Retrieve all possible addresses for the given value and mask.
-// @param value - Memory location
-// @param mask - Bitmask
-const getAddresses = (value: string | number, mask: string) =>
-  resolveAddresses(
-    Array.from($.toBin(+value)).map((char, i) =>
-      mask[i] !== '0' ? mask[i] : char
-    )
-  )
-
-// Strict processor: the mask is used to retrieve possible addresses to write
-// the value at.
-// @param memory - Memory object
-// @param mask - Bitmask
-// @param instruction - Mask (string value) or memory (key,
-//                                        value pair of numbers) instruction
-// @return [memory, mask] Memory and mask for further processing
-export const processStrict = (
-  [memory, mask]: Program,
-  instruction: Instruction
-) => {
-  if (typeof instruction === 'string') return [memory, instruction] as Program
-  getAddresses(instruction[0], mask!).forEach(
-    // @ts-ignore
-    address => (memory[address] = instruction[1])
-  )
-  return [memory, mask] as Program
-}
-
-// Execute the given program with the given processor.
-// @param input - Raw unparsed program input
-// @param processor - Processor to treat every instruction
-export const executeProgram = (
-  input: string[],
-  processor: (acc: Program, value: Instruction) => Program
-) => {
-  const program = parseProgram(input)
-  const [memory] = program.reduce(processor, [{}, null])
-
-  return $.sum(Object.values(memory))
+  return $.sum(Array.from(memory.values()))
 }
--- 2020/14/test.ts
+++ 2020/14/test.ts
@@ -1,6 +1,6 @@
 import test from 'ava'
 import $ from '../../helpers'
-import { parseProgram, executeProgram, processLoose, processStrict } from '.'
+import { run } from '.'
 
 test('Day 14 — Sample', t => {
   const example = $.sample(`
@@ -9,25 +9,19 @@ test('Day 14 — Sample', t => {
   mem[7] = 101
   mem[8] = 0
   `)
-  t.deepEqual(parseProgram(example), [
-    'XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X',
-    [8, 11],
-    [7, 101],
-    [8, 0],
-  ])
-  t.is(executeProgram(example, processLoose), 165)
+  t.is(run(example), 165)
   const example2 = $.sample(`
   mask = 000000000000000000000000000000X1001X
   mem[42] = 100
   mask = 00000000000000000000000000000000X0XX
   mem[26] = 1
   `)
-  t.is(executeProgram(example2, processStrict), 208)
+  t.is(run(example2, true), 208)
 })
 
 test('Day 14 — Solutions', t => {
   const input = $.readInput(import.meta)
 
-  t.is(executeProgram(input, processLoose), 15_172_047_086_292)
-  t.is(executeProgram(input, processStrict), 4_197_941_339_968)
+  t.is(run(input), 15_172_047_086_292)
+  t.is(run(input, true), 4_197_941_339_968)
 })
